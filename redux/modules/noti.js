ad9f8493edd41554f61171be7b8d75ee54f9c6f6
Yeonsoo Joung <ysjoung@uangel.com>
2019-11-29 18:01:22 +0900

    Noti 초기화 추가

--- redux/modules/noti.js
+++ redux/modules/noti.js
@@ -7,16 +7,22 @@ import notiAPI from '../../utils/api/notiApi';
 const  GET_NOTI_LIST =  "rokebi/noti/GET_NOTI_LIST"
 const  READ_NOTI = "rokebi/noti/READ_NOTI"
 const  UPDATE_NOTI =  "rokebi/noti/UPDATE_NOTI"
+const  INIT = "rokebi/noti/INIT"
 
 export const getNotiList = createAction(GET_NOTI_LIST, notiAPI.getNoti)
 export const readNoti = createAction(READ_NOTI, notiAPI.read)
 export const updateNoti = createAction(UPDATE_NOTI, notiAPI.update)
+export const init = createAction(INIT)
 
 const initialState = Map({
   notiList: []
 })
 
 export default handleActions({
+
+  [INIT] : (state, action) => {
+    return initialState
+  },
 
   ... pender({
     type: GET_NOTI_LIST,
