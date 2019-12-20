3be00a09dcf28b868205dea204f871fced2337d7
SOOJEONG1 <soojeongs0408@gmail.com>
2019-12-20 10:31:09 +0900

    profile - 기본배송지설정 관련

--- utils/api/profileApi.js
+++ utils/api/profileApi.js
@@ -4,7 +4,7 @@ import api from './api'
 class ProfileAPI {
     toCustomerProfile = (data) => {
         // JSON API로 데이터를 조회한 경우 
-        if ( ! _.isEmpty(data.jsonapi) && ! _.isEmpty(data.data)) {
+        if ( ! _.isEmpty(data.jsonapi) && ! _.isUndefined(data.data)) {
             const obj = _.isArray(data.data) ? data.data : [data.data]
 
             const objects = obj.map(item => {
@@ -28,6 +28,7 @@ class ProfileAPI {
                     zipCode: postal_code,
                     addressLine1: address_line1,
                     addressLine2: address_line2,
+                    detailAddr: item.field_detail_address,
                     organization: organization,
                     givenName: given_name,
                     familyName: family_name,
@@ -35,10 +36,12 @@ class ProfileAPI {
                     recipient: item.attributes.field_recipient,
                     recipientNumber : item.attributes.field_recipient_number,
                     isBasicAddr: item.attributes.is_default,
+                    detailAddr: item.attributes.field_detail_address,
                     uuid: item.id,
                 }
             
             })
+            console.log('obj', obj)
             return api.success(objects)
         }
         return api.failure(api.NOT_FOUND)        
@@ -60,51 +63,23 @@ class ProfileAPI {
         if ( _.isEmpty(profile) || _.isEmpty(token) ) return api.reject( api.INVALID_ARGUMENT)
         
         const url = api.httpUrl(api.path.jsonapi.profile)
-        const headers = api.withToken(token, 'vnd.api+json')
-        const body = {
-            data : {
-                type: 'profile--customer',
-                attributes: {
-                    address: {
-                        langcode: "ko",
-                        country_code: "KR",
-                        administrative_area: profile.province,   // 경기도 
-                        locality: profile.city,                  // 성남시
-                        postal_code: profile.zipCode,
-                        address_line1: profile.addressLine1,
-                        address_line2: profile.addressLine2 + ' ' + profile.detailAddr,
-                        organization: defaultProfile.organization,
-                        given_name: defaultProfile.givenName || profile.recipient, 
-                        family_name: defaultProfile.familyName || profile.recipient, 
-                    },
-                    field_recipient : profile.recipient,
-                    field_recipient_number : profile.recipientNumber,
-                    field_alias : profile.alias,
-                    field_basic_address : profile.isBasicAddr,
-                    is_default: profile.isBasicAddr,
-                }
-            }
-        }
 
-        console.log('body', body)
-
-        return api.callHttp(url, {
-            method: 'post',
-            headers,
-            body: JSON.stringify(body)
-        }, this.toCustomerProfile)
+        return this.addOrUpdate(url, profile, defaultProfile, 'post', token)
     }
 
-    updateCustomerProfile = ( profile, { token}) => {
-        if ( _.isEmpty(profile) || _.isEmpty(token) ) return api.reject(api.INVALID_ARGUMENT)
-        const url = `${api.httpUrl(api.path.jsonapi.profile)}/${profile.uuid}`
-        const headers = api.withToken(token, 'vnd.api+json', {
+    addOrUpdate = (url, profile, defaultProfile = {}, method, token) => {
+        if ( _.isEmpty(url) || _.isEmpty(profile) || _.isEmpty(method) || _.isEmpty(token) ) return api.reject(api.INVALID_ARGUMENT)
+
+        console.log('profile add or update', profile)
+        const headers = 
+        method=='patch'?
+        api.withToken(token, 'vnd.api+json', {
             'Accept': 'application/vnd.api+json'
-        })
+        }):api.withToken(token, 'vnd.api+json')
         const body = {
             data : {
                 type: 'profile--customer',
-                id: profile.uuid,
+                id: method == 'patch' ? profile.uuid : undefined, // 업로드할 경우에만 필요
                 attributes: {
                     address: {
                         langcode: "ko",
@@ -113,25 +88,31 @@ class ProfileAPI {
                         locality: profile.city,                  // 성남시
                         postal_code: profile.zipCode,
                         address_line1: profile.addressLine1,
-                        address_line2: profile.addressLine2 || ' ' || profile.detailAddr,
-                        organization: profile.organization || profile.alias,
-                        given_name: profile.givenName || profile.recipient.substring(0,1), //'choi',//profile[0].givenName,
-                        family_name: profile.familyName|| profile.recipient.substring(1), //'soojeong', //profile[0].familyName,
+                        address_line2: profile.addressLine2,
+                        organization: defaultProfile.organization ||profile.organization || profile.alias,
+                        given_name: defaultProfile.givenName || profile.givenName || profile.recipient, //.substring(0,1), //'choi',//profile[0].givenName,
+                        family_name: defaultProfile.familyName || profile.familyName|| profile.recipient//.substring(1), //'soojeong', //profile[0].familyName,
                     },
                     field_recipient : profile.recipient,
                     field_recipient_number : profile.recipientNumber,
                     field_alias : profile.alias,
-                    field_basic_address : profile.isBasicAddr,
+                    field_detail_address : profile.detailAddr,
+                    field_selected_address : profile.isBasicAddr,
                     is_default: profile.isBasicAddr
                 }
             }
         }
 
         return api.callHttp(url, {
-            method: 'PATCH',
+            method: method,
             headers,
             body: JSON.stringify(body)
         }, this.toCustomerProfile)
+    }
+    updateCustomerProfile = ( profile, {token}) => {
+        const url = `${api.httpUrl(api.path.jsonapi.profile)}/${profile.uuid}`
+        return this.addOrUpdate( url, profile, {}, 'patch', token)
+
     }
 
     // profile uuid
