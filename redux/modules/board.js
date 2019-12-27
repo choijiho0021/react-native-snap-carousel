c4ddc920283df5ac56a3ee6b1166b08603cf1421
Bohun Tak <bhtak@uangel.com>
2019-12-27 10:45:20 +0900

    board list에서 pull to refresh 기능 추가

--- redux/modules/board.js
+++ redux/modules/board.js
@@ -8,26 +8,51 @@ import {auth} from './account'
 export const POST_ISSUE =   'rokebi/board/POST_ISSUE'
 export const POST_ATTACH =   'rokebi/board/POST_ATTACH'
 const NO_MORE_ISSUES =   'rokebi/board/NO_MORE_ISSUES'
-export const GET_ISSUE_LIST =   'rokebi/board/GET_ISSUE_LIST'
+export const FETCH_ISSUE_LIST =   'rokebi/board/FETCH_ISSUE_LIST'
 export const GET_ISSUE_RESP =   'rokebi/board/GET_ISSUE_RESP'
+const RESET_ISSUE_LIST =   'rokebi/board/RESET_ISSUE_LIST'
+const NEXT_ISSUE_LIST =   'rokebi/board/NEXT_ISSUE_LIST'
 
 export const postIssue = createAction(POST_ISSUE, boardApi.post)
 export const postAttach = createAction(GET_ISSUE_RESP, boardApi.uploadAttachment)
-export const getIssueList = createAction(GET_ISSUE_LIST, boardApi.getIssueList)
+export const fetchIssueList = createAction(FETCH_ISSUE_LIST, boardApi.getIssueList)
 export const getIssueResp = createAction(GET_ISSUE_RESP, boardApi.getComments)
+const resetIssueList = createAction(RESET_ISSUE_LIST)
+const nextIssueList = createAction(NEXT_ISSUE_LIST)
 
-export const getNextIssueList = (uid) => {
+export const getIssueList = () => {
+  return (dispatch,getState) => {
+    const { account } = getState(),
+      uid = account.get('uid'),
+      token = account.get('token')
+
+    dispatch(resetIssueList())
+
+    return dispatch(fetchIssueList(uid, {token}, 0))
+  }
+}
+
+export const getNextIssueList = () => {
   return (dispatch,getState) => {
     const { account, board, pender } = getState(),
       uid = account.get('uid'),
       token = account.get('token'),
       next = board.get('next'),
-      pending = pender.pending[GET_ISSUE_LIST]
+      page = board.get('page'),
+      pending = pender.pending[FETCH_ISSUE_LIST]
 
-    if ( next && ! pending) return dispatch(getIssueList(uid, {token}, next))
-    return dispatch({type: NO_MORE_ISSUES})
+    if (next && !pending) {
+      dispatch(nextIssueList())
+      return dispatch(fetchIssueList(uid, {token}, page+1))
+    }
+
+    // no more list
+    return dispatch({type:NO_MORE_ISSUES})
   }
 }
+
+// 10 items per page
+const PAGE_LIMIT = 10
 
 export const postAndGetList = (issue, attachment) => {
   return (dispatch,getState) => {
@@ -57,7 +82,8 @@ export const postAndGetList = (issue, attachment) => {
 
 
 const initialState = Map({
-  'next': undefined,
+  'next': true,
+  'page' : 0,
   'list': [],
   'comment': undefined
 })
@@ -67,18 +93,26 @@ export default handleActions({
     return state
   },
 
+  [NEXT_ISSUE_LIST] : (state, action) => {
+    return state.update( 'page', value => value +1)
+  },
+
+  [RESET_ISSUE_LIST]: (state, action) => {
+    return state.set('next', true).set('page', 0)
+  },
+
   ... pender({
-    type: GET_ISSUE_LIST,
+    type: FETCH_ISSUE_LIST,
     onSuccess: (state, action) => {
-      const {result, objects, links} = action.payload
+      const {result, objects} = action.payload
       const list = state.get('list')
       if (result == 0 && objects.length > 0) {
-        const next = _.isEmpty(links.next) ? undefined : links.next.href
         const newList = objects.filter(item => list.findIndex(org => org.uuid == item.uuid) < 0)
 
-        return state.set('next', next).set('list', list.concat(newList).sort((a,b) => a.created < b.created ? 1 : -1))
+        return state.set('list', list.concat(newList).sort((a,b) => a.created < b.created ? 1 : -1))
+          .set('next', newList.length == PAGE_LIMIT)
       }
-      return state
+      return state.set('next', false)
     }
   }),
 
