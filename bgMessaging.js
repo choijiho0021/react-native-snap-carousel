7e542f71cff756b6d372d487dbd64441dfe7342c
Yeonsoo Joung <ysjoung@uangel.com>
2020-01-08 16:15:26 +0900

    android background message를 foreground message로 다시한번 호출 추가

--- bgMessaging.js
+++ bgMessaging.js
@@ -5,9 +5,33 @@ export default async (message) => {
     // you can't see this message, because debugger may off when app closed
     // but you can use react native code like fetch, etc ... 
     // const badge = message.data.
-    const badge = message.data.badge
+    const {badge,title,body} = message.data
+    
+    const token = await firebase.messaging().getToken()
     firebase.notifications().setBadge(Number(badge))
-    console.log(message); 
+    console.log("message",message);
 
+    sendPushNotification(token,title,body)
+    
     return Promise.resolve();
+}
+
+const sendPushNotification = async (token,title,body) => {
+  const FIREBASE_API_KEY = 'AIzaSyAzm_xojDYkHmA7IDzE2sjoGpYf-2mzcW0';
+  const message = {
+    to : token,
+    notification: {
+      title,
+      body
+    }
+}
+
+  let headers = new Headers({
+    "Content-Type": "application/json",
+    "Authorization": "key=" + FIREBASE_API_KEY
+  });
+
+  let response = await fetch("https://fcm.googleapis.com/fcm/send", { method: "POST", headers, body: JSON.stringify(message) })
+    .then(response => response.json())
+    console.log("response",response);
 }
