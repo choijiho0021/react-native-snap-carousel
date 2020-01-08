a46b06277a547c88d0dde7aae683189bfd42f52e
Yeonsoo Joung <ysjoung@uangel.com>
2020-01-08 18:03:26 +0900

    ios에서 에러가 발생하므로 android에서만 적용되도록 변경

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import bgMessaging from './bgMessaging';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
+
+if(Platform.OS=='android'){
+  const bgMessaging = require('./bgMessaging')
+  AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
+}
