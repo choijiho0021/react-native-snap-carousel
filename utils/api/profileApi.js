0bf650b548db69eb1b3b1c187741d646ba704552
SOOJEONG1 <soojeongs0408@gmail.com>
2020-02-07 09:14:27 +0900

    주소등록 - dependent_locality 저장하지 않도록 변경

--- utils/api/profileApi.js
+++ utils/api/profileApi.js
@@ -16,7 +16,7 @@ class ProfileAPI {
                     address_line1,
                     address_line2,
                     organization,
-                    dependent_locality,
+                    // dependent_locality,
                     given_name,
                     family_name,
                 } = item.attributes.address
@@ -30,7 +30,7 @@ class ProfileAPI {
                     addressLine1: address_line1,
                     addressLine2: address_line2,
                     detailAddr: organization,
-                    roadAddr: dependent_locality,
+                    // roadAddr: dependent_locality,
                     givenName: given_name,
                     familyName: family_name,
                     alias: item.attributes.field_alias,
@@ -92,7 +92,7 @@ class ProfileAPI {
                         address_line1: profile.addressLine1,
                         address_line2: profile.addressLine2,
                         organization: profile.detailAddr,        // 상세주소
-                        dependent_locality: profile.roadAddr,    // 도로명주소
+                        // dependent_locality: profile.roadAddr,    // 도로명주소
                         given_name: profile.recipient || profile.givenName, 
                         family_name: profile.recipient || profile.familyName
                     },
