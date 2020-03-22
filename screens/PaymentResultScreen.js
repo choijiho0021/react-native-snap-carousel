58ef166993367a4f86fb85710e1d1481e9c804ed
SOOJEONG1 <soojeongs0408@gmail.com>
2020-03-22 17:33:57 +0900

    i18n 추가

--- screens/PaymentResultScreen.js
+++ screens/PaymentResultScreen.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
 class PaymentResultScreen extends Component {
 
   static navigationOptions = ({navigation}) => ({
-    headerLeft: <AppBackButton navigation={navigation} title={i18n.t('cart')} back="top" />
+    headerLeft: <AppBackButton navigation={navigation} title={i18n.t('cart')} back="top"/>
   })
 
   constructor(props){
