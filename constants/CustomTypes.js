56d306ab1b9b8c8816df74f824c80fda783f0e16
SOOJEONG1 <soojeongs0408@gmail.com>
2020-06-11 13:33:44 +0900

    복사클릭할 경우 toast 적용

--- constants/CustomTypes.js
+++ constants/CustomTypes.js
@@ -3,14 +3,16 @@ import { Map } from 'immutable'
 import i18n from '../utils/i18n'
 
 const Toast = {
-    NOT_LOADED  : 1,
-    NOT_UPDATED : 2,
-    NOT_OPENED  : 3,
+    NOT_LOADED      : 1,
+    NOT_UPDATED     : 2,
+    NOT_OPENED      : 3,
+    COPY_SUCCESS    : 4,
 
     messageMap: () => Map({
         [Toast.NOT_LOADED]   : i18n.t('toast:failedToLoad'),
         [Toast.NOT_UPDATED]  : i18n.t('toast:failedToUpdate'),
-        [Toast.NOT_OPENED]   : i18n.t('toast:failedToOpen')
+        [Toast.NOT_OPENED]   : i18n.t('toast:failedToOpen'),
+        [Toast.COPY_SUCCESS]   : i18n.t('toast:copySuccess'),
     }),
 
     mapToMessage: (idx) => {
