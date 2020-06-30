45eb9fc647f55578a02349cde68a9a2f22a2d7f0
Yeonsoo Joung <ysjoung@uangel.com>
2020-06-30 11:21:21 +0900

    Navigation Stack 이름 변경에 따른 에러 수정

--- screens/PaymentResultScreen.js
+++ screens/PaymentResultScreen.js
@@ -131,10 +131,10 @@ class PaymentResultScreen extends Component {
     this.backHandler.remove()
 
     if(key == 'MyPage') {
-      this.props.navigation.reset({routes: [{ name: 'MyPage' }] });
+      this.props.navigation.reset({routes: [{ name: 'MyPageStack' }] });
     }
     else {
-      this.props.navigation.reset({routes: [{ name: 'Home' }] });
+      this.props.navigation.reset({routes: [{ name: 'HomeStack' }] });
     }
   }
 
