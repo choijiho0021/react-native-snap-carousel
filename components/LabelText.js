efac0ea2a8b2f64f37c320832db9e7e92dc90d01
Bohun Tak <bhtak@uangel.com>
2020-08-01 18:15:14 +0900

    code format

--- components/LabelText.js
+++ components/LabelText.js
@@ -1,19 +1,15 @@
-import React, { PureComponent } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import {appStyles} from '../constants/Styles'
+import React, {PureComponent} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {appStyles} from '../constants/Styles';
 import utils from '../utils/utils';
 import i18n from '../utils/i18n';
-import { colors } from '../constants/Colors';
-import _ from 'underscore'
+import {colors} from '../constants/Colors';
+import _ from 'underscore';
 
 const styles = StyleSheet.create({
   label: {
-    ... appStyles.normal14Text,
-    color: colors.warmGrey
+    ...appStyles.normal14Text,
+    color: colors.warmGrey,
   },
   container: {
     alignItems: 'center',
@@ -23,37 +19,54 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   singleValue: {
     flex: 1,
-    textAlign: 'right'
-  }
+    textAlign: 'right',
+  },
 });
 
 export default class LabelText extends PureComponent {
   render() {
-
-    const {label, value = 0, deduct, style, format, color = colors.black, labelStyle, valueStyle} = this.props
-    const isDeduct = label == i18n.t('cart:deductBalance')
+    const {
+      label,
+      value = 0,
+      deduct,
+      style,
+      format,
+      color = colors.black,
+      labelStyle,
+      valueStyle,
+    } = this.props;
+    const isDeduct = label == i18n.t('cart:deductBalance');
 
     return (
-      <View style={[styles.container, style, format != "shortDistance" && {justifyContent: 'space-between'}]} >
-        <Text style={[labelStyle || styles.label, {maxWidth:'70%'}]}>{label}</Text>
+      <View
+        style={[
+          styles.container,
+          style,
+          format != 'shortDistance' && {justifyContent: 'space-between'},
+        ]}>
+        <Text style={[labelStyle || styles.label, {maxWidth: '70%'}]}>
+          {label}
+        </Text>
         {/* {
           isDeduct &&
           <Text style={[styles.label, {marginLeft: 18}]}>{`(${i18n.t('cart:currentBalance')}:${utils.numberToCommaString(value) + ' ' + i18n.t('won')}) `}</Text>
         } */}
-        {
-          ( format == 'price' ) ?
-            <View style={styles.value}>
-              <Text style={[valueStyle|| appStyles.price, {color}]}>{isDeduct && '- '}{utils.numberToCommaString(isDeduct ? deduct : value)}</Text>
-              <Text style={appStyles.normal14Text}>{' ' + i18n.t('won')}</Text>
-            </View>
-            : <Text style={valueStyle || styles.singleValue}>{value}</Text>
-        }
+        {format == 'price' ? (
+          <View style={styles.value}>
+            <Text style={[valueStyle || appStyles.price, {color}]}>
+              {isDeduct && '- '}
+              {utils.numberToCommaString(isDeduct ? deduct : value)}
+            </Text>
+            <Text style={appStyles.normal14Text}>{' ' + i18n.t('won')}</Text>
+          </View>
+        ) : (
+          <Text style={valueStyle || styles.singleValue}>{value}</Text>
+        )}
       </View>
-    )
+    );
   }
 }
-
