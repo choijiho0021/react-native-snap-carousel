05ba5d8b1ebe476acb56cfccf6c31e973aa2dcb3
Bohun Tak <bhtak@uangel.com>
2020-08-01 18:15:00 +0900

    body props 추가

--- components/AppModal.js
+++ components/AppModal.js
@@ -1,184 +1,204 @@
 import React, {PureComponent} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Modal,
-  TextInput,
-} from 'react-native';
-import {appStyles} from "../constants/Styles"
-import i18n from '../utils/i18n'
-import { colors } from '../constants/Colors';
+import {StyleSheet, Text, View, Modal, TextInput} from 'react-native';
+import {appStyles} from '../constants/Styles';
+import i18n from '../utils/i18n';
+import {colors} from '../constants/Colors';
 import AppButton from './AppButton';
 import validationUtil from '../utils/validationUtil';
-import _ from 'underscore'
+import _ from 'underscore';
 
 class AppModal extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       value: props.default,
-      error: undefined
-    }
+      error: undefined,
+    };
 
-    this._onChangeText = this._onChangeText.bind(this)
-    this._onSubmit = this._onSubmit.bind(this)
-    this._renderError = this._renderError.bind(this)
+    this._onChangeText = this._onChangeText.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
+    this._renderError = this._renderError.bind(this);
   }
 
   componentDidUpdate(prevProps) {
-    if ( this.props.visible && this.props.visible != prevProps.visible) {
+    if (this.props.visible && this.props.visible != prevProps.visible) {
       this.setState({
-        value: this.props.default
-      })
+        value: this.props.default,
+      });
     }
   }
 
-  _onChangeText = (key) => (value) => {
+  _onChangeText = key => value => {
     const val = {
-      [key] : value
-    }
-    const error = validationUtil.validateAll(val)
+      [key]: value,
+    };
+    const error = validationUtil.validateAll(val);
 
     this.setState({
-      ... val,
-      error
-    })
-  }
+      ...val,
+      error,
+    });
+  };
 
   async _onSubmit() {
-    const { value } = this.state
-    const validated = (this.props.validate && this.props.validate(value)) ||
-      (this.props.validateAsync && await this.props.validateAsync(value))
+    const {value} = this.state;
+    const validated =
+      (this.props.validate && this.props.validate(value)) ||
+      (this.props.validateAsync && (await this.props.validateAsync(value)));
 
-    console.log('validated', validated)
+    console.log('validated', validated);
 
-    if ( _.isUndefined(validated)) {
+    if (_.isUndefined(validated)) {
       this.setState({
-        showModal: false
-      })
+        showModal: false,
+      });
 
-      this.props.onOkClose(value)
-    }
-    else {
+      this.props.onOkClose(value);
+    } else {
       this.setState({
-        error: validated
-      })
+        error: validated,
+      });
     }
   }
 
   _onCancelClose() {
-    this.props.onCancelClose()
-    this.setState({error:undefined})
+    this.props.onCancelClose();
+    this.setState({error: undefined});
   }
 
   _renderError() {
-    const { error } = this.state
+    const {error} = this.state;
 
-    if ( this.props.mode == 'edit' ) {
-      if ( _.isArray(error)) return (
-        <View>
-        {
-          error.map((err,idx) => <Text key={idx+""} style={styles.error}>{err}</Text>)
-        }
-        </View>
-      )
+    if (this.props.mode == 'edit') {
+      if (_.isArray(error))
+        return (
+          <View>
+            {error.map((err, idx) => (
+              <Text key={idx + ''} style={styles.error}>
+                {err}
+              </Text>
+            ))}
+          </View>
+        );
 
-      if ( ! _.isEmpty(error)) return <Text style={styles.error}>{error}</Text>
+      if (!_.isEmpty(error)) return <Text style={styles.error}>{error}</Text>;
     }
-    return null
+    return null;
   }
 
   render() {
-    const { value, error } = this.state
-    const { title, type, maxLength = undefined, keyboardType = 'default', toRokebiCash = undefined } = this.props
+    const {value, error} = this.state;
+    const {
+      title,
+      body,
+      type,
+      maxLength = undefined,
+      keyboardType = 'default',
+      toRokebiCash = undefined,
+    } = this.props;
 
     return (
-      <Modal animationType="fade"
+      <Modal
+        animationType="fade"
         transparent={false}
-        visible={this.props.visible} >
-
+        visible={this.props.visible}>
         <View style={appStyles.modal}>
           <View style={styles.inner}>
-            {
-              title && <Text style={styles.title}>{title}</Text>
-            }
-            {
-              !_.isUndefined(toRokebiCash) && 
-                <View style={{marginTop: 30}}>
-                  <Text style={styles.blueCenter}>{i18n.t('usim:toRokebiCash')}</Text>
-                  <Text style={styles.blueCenter}>{toRokebiCash} {i18n.t('usim:balance')}</Text>
-                </View>
-            }
-            {
-              this.props.mode == 'edit' && <View style={styles.inputBox}>
-                  <TextInput style={styles.textInput} 
-                    returnKeyType='done'
-                    enablesReturnKeyAutomatically={true}
-                    onChangeText={this._onChangeText('value')}
-                    maxLength={maxLength}
-                    keyboardType={keyboardType}
-                    value={value} /> 
+            {title && <Text style={styles.title}>{title}</Text>}
+            {!_.isUndefined(toRokebiCash) && (
+              <View style={{marginTop: 30}}>
+                <Text style={styles.blueCenter}>
+                  {i18n.t('usim:toRokebiCash')}
+                </Text>
+                <Text style={styles.blueCenter}>
+                  {toRokebiCash} {i18n.t('usim:balance')}
+                </Text>
+              </View>
+            )}
+            {typeof body === 'string' ? (
+              <Text style={styles.body}>{body}</Text>
+            ) : typeof body === 'function' ? (
+              body()
+            ) : null}
+            {this.props.mode == 'edit' && (
+              <View style={styles.inputBox}>
+                <TextInput
+                  style={styles.textInput}
+                  returnKeyType="done"
+                  enablesReturnKeyAutomatically={true}
+                  onChangeText={this._onChangeText('value')}
+                  maxLength={maxLength}
+                  keyboardType={keyboardType}
+                  value={value}
+                />
 
-                  <AppButton style={styles.cancelButton} iconName="btnCancel" onPress={() => this._onChangeText('value')('')}/>
-                </View>
-            }
-            {
-              this._renderError()
-            }
+                <AppButton
+                  style={styles.cancelButton}
+                  iconName="btnCancel"
+                  onPress={() => this._onChangeText('value')('')}
+                />
+              </View>
+            )}
+            {this._renderError()}
 
             <View style={styles.row}>
-              {
-                type === 'info' ? null:
-                  <AppButton style={styles.button} 
-                    onPress={() => this._onCancelClose()}
-                    title={i18n.t('cancel')} 
-                    titleStyle={styles.buttonTitle}/>
-              }
+              {type !== 'info' && (
+                <AppButton
+                  style={styles.button}
+                  onPress={() => this._onCancelClose()}
+                  title={i18n.t('cancel')}
+                  titleStyle={styles.buttonTitle}
+                />
+              )}
 
-              <AppButton style={styles.button} 
-                disabled={! _.isEmpty(error)}
+              <AppButton
+                style={styles.button}
+                disabled={!_.isEmpty(error)}
                 onPress={this._onSubmit}
-                title={i18n.t('ok')} 
+                title={i18n.t('ok')}
                 disableBackgroundColor={colors.white}
                 disableColor={colors.warmGrey}
-                titleStyle={{... styles.buttonTitle, color: _.isEmpty(error) ? colors.clearBlue : colors.warmGrey}}/>
+                titleStyle={{
+                  ...styles.buttonTitle,
+                  color: _.isEmpty(error) ? colors.clearBlue : colors.warmGrey,
+                }}
+              />
             </View>
           </View>
         </View>
       </Modal>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   error: {
-    ... appStyles.normal14Text,
+    ...appStyles.normal14Text,
     color: colors.tomato,
     marginHorizontal: 30,
-    marginTop: 10
+    marginTop: 10,
   },
   cancelButton: {
     width: 20,
     height: 20,
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
   },
   button: {
     backgroundColor: colors.white,
-    width:65,
-    height: 36
+    width: 65,
+    height: 36,
   },
   buttonTitle: {
-    ... appStyles.normal16Text,
+    ...appStyles.normal16Text,
     textAlign: 'right',
-    width: '100%'
+    width: '100%',
   },
   row: {
     marginTop: 40,
     marginHorizontal: 30,
     flexDirection: 'row',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   inputBox: {
     flexDirection: 'row',
@@ -187,27 +207,27 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderBottomColor: colors.black,
     borderBottomWidth: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   textInput: {
-    ... appStyles.normal16Text,
+    ...appStyles.normal16Text,
     flex: 1,
     paddingVertical: 12,
   },
   title: {
-    ... appStyles.normal18Text,
-    marginHorizontal: 30
+    ...appStyles.normal18Text,
+    marginHorizontal: 30,
   },
   blueCenter: {
-    ... appStyles.normal18Text,
+    ...appStyles.normal18Text,
     marginHorizontal: 30,
     color: colors.clearBlue,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   inner: {
-    ... appStyles.modalInner,
-    paddingVertical: 25
-  }
-})
+    ...appStyles.modalInner,
+    paddingVertical: 25,
+  },
+});
 
-export default AppModal
+export default AppModal;
