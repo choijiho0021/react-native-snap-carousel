142b9bdd2d83575d53afccc9fa4f13978d02234a
SOOJEONG1 <soojeongs0408@gmail.com>
2020-08-03 16:47:16 +0900

    앱스토어 등록 - 버전 등

--- environment.js
+++ environment.js
@@ -3,7 +3,7 @@ import {Platform} from 'react-native';
 const impId = 'imp53913318';
 
 // rokebi esim App
-const appId = 'rokebi';
+const appId = 'esim';
 const codePushiOSStagLabel = 'v123';
 const codePushiOSProdLabel = 'v0';
 const codePushAndStagLabel = 'v116';
@@ -20,14 +20,14 @@ const ENV = {
   esim: {
     dev: {
       ...env,
-      // scheme: 'https',
-      // rokApiUrl: "service.rokebi.com",
-      // apiUrl: "api.rokebi.com",
-      // baseUrl: "https://www.rokebi.com",
-      scheme: 'http',
-      rokApiUrl: 'tb.service.rokebi.com',
-      apiUrl: 'esim-tb-v1.ap-northeast-2.elasticbeanstalk.com',
-      baseUrl: 'http://esim-tb-v1.ap-northeast-2.elasticbeanstalk.com',
+      scheme: 'https',
+      rokApiUrl: 'service.rokebi.com',
+      apiUrl: 'esim.rokebi.com',
+      baseUrl: 'https://esim.rokebi.com',
+      // scheme: 'http',
+      // rokApiUrl: 'tb.service.rokebi.com',
+      // apiUrl: 'esim-tb-v1.ap-northeast-2.elasticbeanstalk.com',
+      // baseUrl: 'http://esim-tb-v1.ap-northeast-2.elasticbeanstalk.com',
       label:
         Platform.OS === 'ios' ? codePushiOSStagLabel : codePushAndStagLabel,
     },
