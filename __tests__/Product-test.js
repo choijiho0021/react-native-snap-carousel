14d94c126add0b8ed9bdd152136898738291238b
Bohun Tak <bhtak@uangel.com>
2020-08-18 00:02:37 +0900

    product-test 추가

--- __tests__/Product-test.js
+++ __tests__/Product-test.js
@@ -1,15 +1,30 @@
-import productApi from '../utils/api/productApi'
-import 'isomorphic-fetch'
+import {API} from '../submodules/rokebi-utils';
+import 'isomorphic-fetch';
 
 describe('Product API', () => {
-  it(`get product by country`, () => {
-    return productApi.getProduct().then(resp =>  {
-      console.log('resp', resp)
-      expect(resp.result).toEqual(0)
-      expect(resp.objects.length).toBeGreaterThan(0)
-    }).catch( err => {
-      console.log('failed to get products', err)
-      expect(err).toBeNull()
-    })
+  it(`전체 상품 목록을 가져온다.`, () => {
+    return API.Product.getProduct()
+      .then(resp => {
+        expect(resp.result).toEqual(0);
+        expect(resp.objects.length).toBeGreaterThan(0);
+        [
+          'key',
+          'uuid',
+          'name',
+          'price',
+          'field_daily',
+          'partnerId',
+          'categoryId',
+          'days',
+          'variationId',
+          'field_description',
+          'promoFlag',
+          'sku',
+          'idx',
+        ].forEach(key => expect(resp.objects[0]).toHaveProperty(key));
+      })
+      .catch(err => {
+        expect(err).toBeNull();
+      });
   });
 });
