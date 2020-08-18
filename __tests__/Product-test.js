855af2ab72b1434a74fda124a59f149f27ea9f4a
Bohun Tak <bhtak@uangel.com>
2020-08-18 17:32:03 +0900

    update test cases

--- __tests__/Product-test.js
+++ __tests__/Product-test.js
@@ -7,7 +7,7 @@ const sku = 'rm-kr-3-1g';
 const auth = {
   token,
   iccid: '1234512345123451234',
-  mail: 'test@test.com',
+  mail: '',
   user: '01010002000',
   pass: '000000',
   cookie: '',
@@ -49,9 +49,34 @@ describe('로그인 후 테스트', () => {
     expect(resp.objects[0]).toHaveProperty('csrf_token');
     expect(resp.objects[0]).toHaveProperty('current_user');
     expect(resp.objects[0]).toHaveProperty('cookie');
+
     const cookie = resp.objects[0].cookie;
     auth.cookie = cookie.substr(0, cookie.indexOf(';'));
     auth.token = resp.objects[0].csrf_token;
+  });
+
+  it('User 확인 : ', async () => {
+    const resp = await API.User.getByName(auth.user, auth);
+    console.log('user', resp);
+    expect(resp.result).toEqual(0);
+    expect(resp.objects.length).toBeGreaterThan(0);
+    expect(resp.objects[0]).toHaveProperty('mail');
+    auth.mail = resp.objects[0].mail;
+  });
+
+  it('ICCID 확인 : ' + auth.iccid, async () => {
+    const resp = await API.Account.getByUser(auth.user, auth);
+    expect(resp.result).toEqual(0);
+    expect(resp.objects.length).toBeGreaterThan(0);
+    expect(resp.objects[0]).toHaveProperty('iccid', auth.iccid);
+  });
+
+  it('ICCID Account 확인 : ' + auth.iccid, async () => {
+    const resp = await API.Account.getAccount(auth.iccid, auth);
+    expect(resp.result).toEqual(0);
+    expect(resp.objects.length).toBeGreaterThan(0);
+    expect(resp.objects[0]).toHaveProperty('iccid', auth.iccid);
+    expect(resp.objects[0]).toHaveProperty('mobile', auth.user);
   });
 
   it('check variables', () => {
@@ -59,19 +84,28 @@ describe('로그인 후 테스트', () => {
     expect(prod).not.toBeNull();
   });
 
-  it('상품을 구매한다:' + sku, async () => {
+  it.skip('상품을 구매한다:' + sku, async () => {
     const purchaseItem = API.Product.toPurchaseItem(prod);
     const paymentResult = PaymentResult.createForRokebiCash({
       impId: 'test',
       mobile: auth.user,
-      profileId: '0',
       deduct: 1,
       dlvCost: 0,
       memo: 'test',
       digital: true,
     });
     const resp = await API.Cart.makeOrder([purchaseItem], paymentResult, auth);
-    console.log('purchase', auth, resp);
     expect(resp.result).toEqual(0);
+  });
+
+  it('구매한 상품을 조회한다.', async () => {
+    const resp = await API.Order.getOrders(auth);
+    console.log('resp', prod, resp.objects[0].orderItems);
+
+    expect(resp.result).toEqual(0);
+    expect(resp.objects.length).toBeGreaterThan(0);
+    expect(resp.objects[0]).toHaveProperty('orderItems');
+    expect(resp.objects[0].orderItems.length).toBeGreaterThan(0);
+    expect(resp.objects[0].orderItems[0]).toHaveProperty('title', prod.name);
   });
 });
