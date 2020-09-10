394cfc48b148294cda3b20c8719a2f9d8e79c5e3
Jisong <jisonglee@uangel.com>
2020-09-10 16:09:41 +0900

    code push: sim type도 같이 입력하도록 변경

--- code_push.sh
+++ code_push.sh
@@ -3,6 +3,7 @@
 cmd=$1
 environment=$2
 environment="$(tr '[:lower:]' '[:upper:]' <<< ${environment:0:1})${environment:1}"
+simtype=$(echo "$3" | tr '[:upper:]' '[:lower:]')
 
 _dir="`pwd`"
 
@@ -16,9 +17,25 @@ if [[ ! "$environment" =~ ^(Staging|Production)$ ]]; then
  	exit 1
 fi
 
+if [[ ! "$simtype" =~ ^(usim|esim)$ ]]; then
+ 	echo "\033[31m"[Error]"\033[0m" "Please choose Sim Type (ESIM|USIM)"
+ 	exit 1
+fi
+
 if [[ "$cmd" = "promote" && "$environment" = "Staging" ]]; then
 	echo "\033[31m"[Error]"\033[0m" "You can only promote the package for production"
 	exit 1
+fi
+
+account='admin-uangel.kr'
+appnameios='Rokebi-iOS'
+appnameandroid='Rokebi-Android'
+xcodeproj='Rokebi.xcodeproj'
+
+if [[ "$simtype" = "esim" ]]; then
+	appnameios='RokebiEsim-iOS'
+	appnameandroid='RokebiEsim-Android'
+	xcodeproj='RokebiESIM.xcodeproj'
 fi
 
 echo "\033[32m"[Info]"\033[0m" "Please enter the Android/iOS version manually before executing the command."
@@ -26,7 +43,7 @@ echo "\033[32m"[Info]"\033[0m" "Please check the CodePush Key again before pushi
 
 echo ""
 
-targetiOSBinaryVersion=`grep 'MARKETING_VERSION' $_dir/ios/RokebiESIM.xcodeproj/project.pbxproj | tail -1 | awk  -F " |;" '{print $3}' `
+targetiOSBinaryVersion=`grep 'MARKETING_VERSION' $_dir/ios/$xcodeproj/project.pbxproj | tail -1 | awk  -F " |;" '{print $3}' `
 
 androidMajorVersion=`grep 'VERSION_MAJOR' $_dir/android/app/version.properties | awk  -F "=" '{print $2}' `
 androidMinorVersion=`grep 'VERSION_MINOR' $_dir/android/app/version.properties | awk  -F "=" '{print $2}' `
@@ -35,8 +52,8 @@ targetAndroidBinaryVersion=`echo $androidMajorVersion.$androidMinorVersion`
 echo "\033[32m"[Info]"\033[0m" "Target iOS Binary version:            $targetiOSBinaryVersion"
 echo "\033[32m"[Info]"\033[0m" "Target Android Binary version:        $targetAndroidBinaryVersion"
 
-curiOSVersion=`appcenter codepush deployment list -a admin-uangel.kr/RokebiEsim-iOS | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
-curAndroidVersion=`appcenter codepush deployment list -a admin-uangel.kr/RokebiEsim-Android | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
+curiOSVersion=`appcenter codepush deployment list -a $account/$appnameios | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
+curAndroidVersion=`appcenter codepush deployment list -a $account/$appnameandroid | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
 
 if [[ ! "$curiOSVersion" =~ ^v[0-9]+$ ]] || [[ ! "$curAndroidVersion" =~ ^v[0-9]+$ ]]; then
 	echo "\033[31m"[Error]"\033[0m" "Please check appcenter-cli first"
@@ -48,8 +65,8 @@ echo "\033[32m"[Info]"\033[0m" "Current $environment Android version: $curAndroi
 
 echo "\033[32m"[Info]"\033[0m" "Get CodePush Key($environment) from App Center"
 
-CODEPUSH_IOS_KEY=`appcenter codepush deployment list -k --app  admin-uangel.kr/RokebiEsim-iOS | grep $environment | sed -e 's/'"${environment}"'//g' -e 's/ //g' -e 's/│//g'`
-CODEPUSH_ANDROID_KEY=`appcenter codepush deployment list -k --app  admin-uangel.kr/RokebiEsim-Android | grep $environment | sed -e 's/'"${environment}"'//g' -e 's/ //g' -e 's/│//g'`
+CODEPUSH_IOS_KEY=`appcenter codepush deployment list -k --app  $account/$appnameios | grep $environment | sed -e 's/'"${environment}"'//g' -e 's/ //g' -e 's/│//g'`
+CODEPUSH_ANDROID_KEY=`appcenter codepush deployment list -k --app  $account/$appnameandroid | grep $environment | sed -e 's/'"${environment}"'//g' -e 's/ //g' -e 's/│//g'`
 
 CODEPUSH_IOS_KEY=`echo $CODEPUSH_IOS_KEY | cat -v | sed -e 's/\^\[\[39m//g' -e 's/\^\[\[90m//g'`
 CODEPUSH_ANDROID_KEY=`echo $CODEPUSH_ANDROID_KEY | cat -v | sed -e 's/\^\[\[39m//g' -e 's/\^\[\[90m//g'`
@@ -60,11 +77,11 @@ then
 	exit 1
 fi
 
-sed -i '' -E 's/CODEPUSH_KEY ?= ?.+/CODEPUSH_KEY = '"${CODEPUSH_IOS_KEY}"';/g' $_dir/ios/RokebiESIM.xcodeproj/project.pbxproj
+sed -i '' -E 's/CODEPUSH_KEY ?= ?.+/CODEPUSH_KEY = '"${CODEPUSH_IOS_KEY}"';/g' $_dir/ios/$xcodeproj/project.pbxproj
 sed -i '' -E 's/buildConfigField "String", "CODEPUSH_KEY", '\''".+"'\''/buildConfigField "String", "CODEPUSH_KEY", '\'\"''"$CODEPUSH_ANDROID_KEY"''\"\''/' $_dir/android/app/build.gradle
 
-sed -i '' -E 's/\$\(CODEPUSH_KEY\)/'"${CODEPUSH_IOS_KEY}"'/g' $_dir/ios/RokebiESIM/Info.plist
-sed -i '' -E 's/\$\(CODEPUSH_KEY\)/'"${CODEPUSH_IOS_KEY}"'/g' $_dir/ios/RokebiESIM/Development.plist
+sed -i '' -E 's/\$\(CODEPUSH_KEY\)/'"${CODEPUSH_IOS_KEY}"'/g' $_dir/ios/Rokebi/Info.plist
+sed -i '' -E 's/\$\(CODEPUSH_KEY\)/'"${CODEPUSH_IOS_KEY}"'/g' $_dir/ios/Rokebi/Development.plist
 
 # 버전 변경 (수동으로 진행 필요)
 
@@ -85,24 +102,24 @@ case $cmd in
  	 		sed -i '' -E 's/  stagingIOS: ?.+/  stagingIOS: "'"${nextiOSVersion}"'",/g'  $_dir/environment.js
 		fi
 	
-		appcenter codepush release-react -a admin-uangel.kr/RokebiEsim-iOS -t $targetiOSBinaryVersion -d $environment
-		appcenter codepush release-react -a admin-uangel.kr/RokebiEsim-Android -t $targetAndroidBinaryVersion -d $environment
+		appcenter codepush release-react -a $account/$appnameios -t $targetiOSBinaryVersion -d $environment
+		appcenter codepush release-react -a $account/$appnameandroid -t $targetAndroidBinaryVersion -d $environment
 
 		;;
 
 	patch)
 		echo "\033[32m"[Info]"\033[0m" "Update the metadata for the existing release  ($environment)"
 	
-		appcenter codepush patch -a admin-uangel.kr/RokebiEsim-iOS $environment -l $curiOSVersion -t $targetiOSBinaryVersion
-		appcenter codepush patch -a admin-uangel.kr/RokebiEsim-Android $environment -l $curAndroidVersion -t $targetAndroidBinaryVersion
+		appcenter codepush patch -a $account/$appnameios $environment -l $curiOSVersion -t $targetiOSBinaryVersion
+		appcenter codepush patch -a $account/$appnameandroid $environment -l $curAndroidVersion -t $targetAndroidBinaryVersion
 
 		;;
 
 	promote)
 		echo "\033[32m"[Info]"\033[0m" "Create a new release for ($environment) from the latest release of staging deployment"
 
-		appcenter codepush promote -a admin-uangel.kr/RokebiEsim-iOS -s Staging -d Production
-		appcenter codepush promote -a admin-uangel.kr/RokebiEsim-Android -s Staging -d Production
+		appcenter codepush promote -a $account/$appnameios -s Staging -d Production
+		appcenter codepush promote -a $account/$appnameandroid -s Staging -d Production
 
 		;;
 	*) ;;
@@ -110,16 +127,16 @@ esac
 
 echo ""
 
-newiOSVersion=`appcenter codepush deployment list -a admin-uangel.kr/RokebiEsim-iOS | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
-newAndroidVersion=`appcenter codepush deployment list -a admin-uangel.kr/RokebiEsim-Android | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
+newiOSVersion=`appcenter codepush deployment list -a $account/$appnameios | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
+newAndroidVersion=`appcenter codepush deployment list -a $account/$appnameandroid | grep $environment | grep 'Label' | awk -F " " '{print $5}'`
 
 echo "\033[32m"[Info]"\033[0m" "Current $environment iOS version:     $newiOSVersion"
 echo "\033[32m"[Info]"\033[0m" "Current $environment Android version: $newAndroidVersion"
 
 # 소스 롤백
 
-sed -i '' -E 's/'"${CODEPUSH_IOS_KEY}"'/\$\(CODEPUSH_KEY\)/g' ./ios/RokebiESIM/Info.plist
-sed -i '' -E 's/'"${CODEPUSH_IOS_KEY}"'/\$\(CODEPUSH_KEY\)/g' ./ios/RokebiESIM/Development.plist
+sed -i '' -E 's/'"${CODEPUSH_IOS_KEY}"'/\$\(CODEPUSH_KEY\)/g' ./ios/Rokebi/Info.plist
+sed -i '' -E 's/'"${CODEPUSH_IOS_KEY}"'/\$\(CODEPUSH_KEY\)/g' ./ios/Rokebi/Development.plist
 
 if [ "$environment" = "Production" ]; then
 	sed -i '' -E 's/  productionAndroid: ?.+/  productionAndroid: "'"${newAndroidVersion}"'",/g'  $_dir/environment.js
@@ -128,5 +145,3 @@ else
 	sed -i '' -E 's/  stagingAndroid: ?.+/  stagingAndroid: "'"${newAndroidVersion}"'",/g'  $_dir/environment.js
 	sed -i '' -E 's/  stagingIOS: ?.+/  stagingIOS: "'"${newiOSVersion}"'",/g'  $_dir/environment.js
 fi
-
-
