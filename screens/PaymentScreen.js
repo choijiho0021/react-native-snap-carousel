f9635b4b9d2adb683a8c5c9c9cb50617989eeecd
SOOJEONG1 <soojeongs0408@gmail.com>
2021-03-15 20:01:10 +0900

    reformat code, 타앱결제취소 버그 수정

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,139 +1,19 @@
+import IMP from 'iamport-react-native';
 import React, {Component} from 'react';
-import {View, StyleSheet, SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Video from 'react-native-video';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as cartActions from '../redux/modules/cart';
-import Video from 'react-native-video';
-import Env from '../environment';
-import i18n from '../utils/i18n';
-import AppBackButton from '../components/AppBackButton';
-import IMP from 'iamport-react-native';
 import _ from 'underscore';
-import api from '../submodules/rokebi-utils/api/api';
 import AppAlert from '../components/AppAlert';
+import AppBackButton from '../components/AppBackButton';
+import Env from '../environment';
+import * as cartActions from '../redux/modules/cart';
+import api from '../submodules/rokebi-utils/api/api';
+import i18n from '../utils/i18n';
 
 // const IMP = require('iamport-react-native').default;
-
-class PaymentScreen extends Component {
-  constructor(props) {
-    super(props);
-
-    const {params = {}} = this.props.route;
-    this.props.navigation.setOptions({
-      title: null,
-      headerLeft: () => (
-        <AppBackButton
-          navigation={this.props.navigation}
-          title={
-            params.isPaid ? i18n.t('his:paymentCompleted') : i18n.t('payment')
-          }
-          isPaid={params.isPaid}
-          pymResult={params.pymResult}
-          orderResult={params.orderResult}
-        />
-      ),
-    });
-
-    this.state = {
-      params: {},
-      isPaid: true,
-    };
-
-    this._callback = this._callback.bind(this);
-  }
-
-  componentDidMount() {
-    const params = this.props.route.params && this.props.route.params.params;
-    if (this.state.isPaid) {
-      this.setState({
-        isPaid: false,
-      });
-      this.props.navigation.setParams({isPaid: false});
-    }
-
-    if (params.mode == 'test' || params.amount == 0) {
-      const {impId} = Env.get();
-      const response = {
-        success: true,
-        imp_uid: impId,
-        merchant_uid: params.merchant_uid,
-        amount: params.amount,
-        profile_uuid: params.profile_uuid,
-        rokebi_cash: params.rokebi_cash,
-        memo: params.memo,
-      };
-
-      this._callback(response);
-    }
-  }
-
-  async _callback(response) {
-    const success = _.isEmpty(response.success) ? true : response.success;
-    if (success) {
-      // 결제완료시 '다음' 버튼 연속클릭 방지 - 연속클릭시 추가 결제 없이 order 계속 생성
-      if (!this.props.route.params.isPaid) {
-        await this.props.navigation.setParams({isPaid: true});
-        const params =
-          this.props.route.params && this.props.route.params.params;
-        this.props.action.cart
-          .payNorder({
-            ...response,
-            pg_provider: params.pg,
-            payment_type: params.pay_method,
-            amount: params.amount,
-            profile_uuid: params.profile_uuid,
-            rokebi_cash: params.rokebi_cash,
-            dlvCost: params.dlvCost,
-            memo: params.memo,
-          })
-          .then(resp => {
-            if (resp.result == 0) {
-              this.props.navigation.replace('PaymentResult', {
-                pymResult: response,
-                orderResult: resp,
-              });
-            } else {
-              if (resp.result === api.E_RESOURCE_NOT_FOUND) {
-                AppAlert.info(resp.title + i18n.t(message));
-              } else {
-                AppAlert.info(i18n.t('cart:systemError'));
-              }
-              this.props.navigation.goBack();
-            }
-          });
-      }
-    } else {
-      this.props.navigation.goBack();
-    }
-  }
-
-  render() {
-    const {impId} = Env.get();
-    const params = this.props.route.params && this.props.route.params.params;
-
-    return (
-      <SafeAreaView
-        style={styles.container}
-        forceInset={{top: 'never', bottom: 'always'}}>
-        <IMP.Payment
-          userCode={impId}
-          loading={
-            <Video
-              source={require('../assets/images/loading_1.mp4')}
-              repeat={true}
-              style={styles.backgroundVideo}
-              resizeMode="cover"
-            />
-          }
-          startInLoadingState={true}
-          data={params} // 결제 데이터
-          callback={this._callback}
-          style={styles.webview}
-        />
-      </SafeAreaView>
-    );
-  }
-}
+const loading = require('../assets/images/loading_1.mp4');
 
 const styles = StyleSheet.create({
   container: {
@@ -158,12 +38,135 @@ const styles = StyleSheet.create({
     right: 0,
   },
 });
+class PaymentScreen extends Component {
+  constructor(props) {
+    super(props);
 
-export default connect(
-  undefined,
-  dispatch => ({
-    action: {
-      cart: bindActionCreators(cartActions, dispatch),
-    },
-  }),
-)(PaymentScreen);
+    const {params = {}} = this.props.route;
+    this.props.navigation.setOptions({
+      title: null,
+      headerLeft: () => (
+        <AppBackButton
+          navigation={this.props.navigation}
+          title={
+            params.isPaid ? i18n.t('his:paymentCompleted') : i18n.t('payment')
+          }
+          isPaid={params.isPaid}
+          pymResult={params.pymResult}
+          orderResult={params.orderResult}
+        />
+      ),
+    });
+
+    this.state = {
+      isPaid: true,
+    };
+
+    this.callback = this.callback.bind(this);
+    this.isTrue = this.isTrue.bind(this);
+  }
+
+  componentDidMount() {
+    const params = this.props.route.params && this.props.route.params.params;
+    if (this.state.isPaid) {
+      this.setState({
+        isPaid: false,
+      });
+      this.props.navigation.setParams({isPaid: false});
+    }
+
+    if (params.mode === 'test' || params.amount === 0) {
+      const {impId} = Env.get();
+      const response = {
+        success: true,
+        imp_uid: impId,
+        merchant_uid: params.merchant_uid,
+        amount: params.amount,
+        profile_uuid: params.profile_uuid,
+        rokebi_cash: params.rokebi_cash,
+        memo: params.memo,
+      };
+
+      this.callback(response);
+    }
+  }
+
+  isTrue = (val) => {
+    return val === 'true';
+  };
+
+  async callback(response) {
+    const success =
+      Array.isArray(response.success) || _.isUndefined(response.success)
+        ? this.isTrue(response.imp_success)
+        : response.success;
+
+    if (success) {
+      // 결제완료시 '다음' 버튼 연속클릭 방지 - 연속클릭시 추가 결제 없이 order 계속 생성
+      if (!this.props.route.params.isPaid) {
+        await this.props.navigation.setParams({isPaid: true});
+        const params =
+          this.props.route.params && this.props.route.params.params;
+        this.props.action.cart
+          .payNorder({
+            ...response,
+            pg_provider: params.pg,
+            payment_type: params.pay_method,
+            amount: params.amount,
+            profile_uuid: params.profile_uuid,
+            rokebi_cash: params.rokebi_cash,
+            dlvCost: params.dlvCost,
+            memo: params.memo,
+          })
+          .then((resp) => {
+            if (resp.result === 0) {
+              this.props.navigation.replace('PaymentResult', {
+                pymResult: response,
+                orderResult: resp,
+              });
+            } else {
+              if (resp.result === api.E_RESOURCE_NOT_FOUND)
+                AppAlert.info(`${resp.title} ${i18n.t('cart:soldOut')}`);
+              else AppAlert.info(i18n.t('cart:systemError'));
+              this.props.navigation.goBack();
+            }
+          });
+      }
+    } else {
+      this.props.navigation.goBack();
+    }
+  }
+
+  render() {
+    const {impId} = Env.get();
+    const params = this.props.route.params && this.props.route.params.params;
+
+    return (
+      <SafeAreaView
+        style={styles.container}
+        forceInset={{top: 'never', bottom: 'always'}}>
+        <IMP.Payment
+          userCode={impId}
+          loading={
+            <Video
+              source={loading}
+              repeat
+              style={styles.backgroundVideo}
+              resizeMode="cover"
+            />
+          }
+          startInLoadingState
+          data={params} // 결제 데이터
+          callback={this.callback}
+          style={styles.webview}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+export default connect(undefined, (dispatch) => ({
+  action: {
+    cart: bindActionCreators(cartActions, dispatch),
+  },
+}))(PaymentScreen);
