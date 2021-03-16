04f18441ad6b53b9ed99148557dc5fbd89e3a696
SOOJEONG1 <soojeongs0408@gmail.com>
2021-03-16 10:16:42 +0900

    setoptions 이동

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -42,22 +42,6 @@ class PaymentScreen extends Component {
   constructor(props) {
     super(props);
 
-    const {params = {}} = this.props.route;
-    this.props.navigation.setOptions({
-      title: null,
-      headerLeft: () => (
-        <AppBackButton
-          navigation={this.props.navigation}
-          title={
-            params.isPaid ? i18n.t('his:paymentCompleted') : i18n.t('payment')
-          }
-          isPaid={params.isPaid}
-          pymResult={params.pymResult}
-          orderResult={params.orderResult}
-        />
-      ),
-    });
-
     this.state = {
       isPaid: true,
     };
@@ -68,6 +52,24 @@ class PaymentScreen extends Component {
 
   componentDidMount() {
     const params = this.props.route.params && this.props.route.params.params;
+
+    this.props.navigation.setOptions({
+      title: null,
+      headerLeft: () => (
+        <AppBackButton
+          navigation={this.props.navigation}
+          title={
+            (params || {}).isPaid
+              ? i18n.t('his:paymentCompleted')
+              : i18n.t('payment')
+          }
+          isPaid={params.isPaid}
+          pymResult={params.pymResult}
+          orderResult={params.orderResult}
+        />
+      ),
+    });
+
     if (this.state.isPaid) {
       this.setState({
         isPaid: false,
