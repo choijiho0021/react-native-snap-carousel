c209b70f89621e8519fb178f821464484c2863be
Yeonsoo Joung <ysjoung@uangel.com>
2021-03-16 17:45:44 +0900

    productdetail reformat code

--- screens/ProductDetailScreen.js
+++ screens/ProductDetailScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import React, {Component} from 'react';
 import {
   StyleSheet,
@@ -12,23 +13,23 @@ import {
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
+import _ from 'underscore';
+import WebView from 'react-native-webview';
+import Analytics from 'appcenter-analytics';
+import KakaoSDK from '@actbase/react-native-kakaosdk';
+import {API} from '../submodules/rokebi-utils';
 import i18n from '../utils/i18n';
 import AppBackButton from '../components/AppBackButton';
-import _ from 'underscore';
 import AppActivityIndicator from '../components/AppActivityIndicator';
 import {colors} from '../constants/Colors';
 import {appStyles, htmlDetailWithCss} from '../constants/Styles';
 import AppButton from '../components/AppButton';
-import WebView from 'react-native-webview';
 import Env from '../environment';
-import Analytics from 'appcenter-analytics';
-import KakaoSDK from '@actbase/react-native-kakaosdk';
 import {windowWidth} from '../constants/SliderEntry.style';
 import * as toastActions from '../redux/modules/toast';
 import * as productActions from '../redux/modules/product';
 import {Toast} from '../constants/CustomTypes';
 import AppIcon from '../components/AppIcon';
-import {API} from 'RokebiESIM/submodules/rokebi-utils';
 
 const {channelId} = Env.get();
 
@@ -37,6 +38,52 @@ const TAB_IDX_ASK_BY_KAKAO = 3; // KakaoTalk으로 물어보기 Tab의 index
 const {baseUrl} = Env.get();
 
 const tabList = ['ProdInfo', 'Tip', 'Caution', 'Ask with KakaoTalk'];
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  tabView: {
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  },
+  whiteBackground: {
+    backgroundColor: colors.white,
+  },
+  normal16WarmGrey: {
+    ...appStyles.normal16Text,
+    color: colors.warmGrey,
+  },
+  boldClearBlue: {
+    color: colors.clearBlue,
+    fontWeight: 'bold',
+  },
+  questionImage: {
+    marginBottom: 14,
+    marginTop: 45,
+  },
+  kakaoImage: {},
+  kakaoContainer: {
+    alignItems: 'center',
+    backgroundColor: colors.whiteTwo,
+    height: 350,
+  },
+  kakaoPlus: {
+    ...appStyles.normal14Text,
+    marginTop: 56,
+    marginBottom: 10,
+    color: colors.warmGrey,
+  },
+});
 
 // document.body.clientWidth : 화면의 너비
 // document.documentElement.clientHeight : 문서의 총 높이
@@ -86,6 +133,28 @@ class ProductDetailScreen extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      scrollY: new Animated.Value(0),
+      tabIdx: 0,
+      querying: true,
+    };
+
+    this.toastRef = React.createRef();
+
+    this.openKTalk = this.openKTalk.bind(this);
+    this.checkIdx = this.checkIdx.bind(this);
+    this.scrollTo = this.scrollTo.bind(this);
+    this.renderContactKakao = this.renderContactKakao.bind(this);
+    this.renderWebView = this.renderWebView.bind(this);
+    this.onMessage = this.onMessage.bind(this);
+    this.checkWindowSize = this.checkWindowSize.bind(this);
+    this.clickTab = this.clickTab.bind(this);
+
+    this.controller = new AbortController();
+    this.scrollView = React.createRef();
+  }
+
+  componentDidMount() {
     this.props.navigation.setOptions({
       title: null,
       headerLeft: () => (
@@ -96,25 +165,8 @@ class ProductDetailScreen extends Component {
       ),
     });
 
-    this.state = {
-      scrollY: new Animated.Value(0),
-      tabIdx: 0,
-      querying: true,
-    };
-
-    this._toastRef = React.createRef();
-
-    this._openKTalk = this._openKTalk.bind(this);
-    this.checkIdx = this.checkIdx.bind(this);
-    this._scrollTo = this._scrollTo.bind(this);
-    this.renderContactKakao = this.renderContactKakao.bind(this);
-    this.renderWebView = this.renderWebView.bind(this);
-    this._onMessage = this._onMessage.bind(this);
-    this._checkWindowSize = this._checkWindowSize.bind(this);
-    this._clickTab = this._clickTab.bind(this);
-
-    this.controller = new AbortController();
-    this.scrollView = React.createRef();
+    // todo : 상세 HTML을 가져오도록 변경 필요
+    this.props.action.product.getProdDetail(this.controller);
   }
 
   shouldComponentUpdate(preProps, preState) {
@@ -122,78 +174,98 @@ class ProductDetailScreen extends Component {
     const {detail} = this.props.product;
 
     return (
-      preState.tabIdx != tabIdx ||
-      preState.height2 != height2 ||
-      preProps.detail != detail ||
-      preProps.localOpDetails != this.props.localOpDetails
+      preState.tabIdx !== tabIdx ||
+      preState.height2 !== height2 ||
+      preProps.detail !== detail ||
+      preProps.localOpDetails !== this.props.localOpDetails
     );
   }
 
-  componentDidMount() {
-    //todo : 상세 HTML을 가져오도록 변경 필요
-    this.props.action.product.getProdDetail(this.controller);
-  }
+  onMessage(event) {
+    const cmd = JSON.parse(event.nativeEvent.data);
 
-  _checkWindowSize(sizeString = '') {
-    const sz = sizeString.split(','),
-      scale = windowWidth / Number(sz[0]);
-
-    // console.log('@@@ height', sizeString, scale)
-
-    for (var i = 2; i < sz.length; i++) {
-      // 각 tab별로 시작 위치를 설정한다.
-      this.setState({
-        ['height' + (i - 2)]: Math.ceil(Number(sz[i]) * scale),
-      });
-    }
-    // 전체 화면의 높이를 저장한다.
-    this.setState({
-      ['height' + (i - 2)]: Math.ceil(Number(sz[1]) * scale),
-    });
-  }
-
-  checkIdx(event) {
-    var {contentOffset} = event,
-      offset = contentOffset.y;
-
-    // console.log('@@@ offset', offset, event)
-
-    offset -= HEADER_IMG_HEIGHT;
-    if (this.state.tabIdx != TAB_IDX_ASK_BY_KAKAO) {
-      for (var idx = 0; idx < TAB_IDX_ASK_BY_KAKAO; idx++) {
-        // 어떤 tab 위치를 스크롤하고 있는지 계산한다.
-        if (offset < this.state['height' + (idx + 1)]) {
-          if (this.state.tabIdx != idx) this.setState({tabIdx: idx});
-          break;
+    switch (cmd.key) {
+      case 'dimension':
+        this.checkWindowSize(cmd.value);
+        break;
+      case 'move':
+        if (cmd.value) {
+          const moveTo = cmd.value.split('/');
+          this.props.navigation.navigate('Faq', {
+            key: moveTo[0],
+            num: moveTo[1],
+          });
         }
-      }
+        break;
+      case 'copy':
+        this.props.action.toast.push(Toast.COPY_SUCCESS);
+        break;
+      default:
+        Clipboard.setString(cmd.value);
     }
   }
 
-  _scrollTo(y) {
-    // console.log('@@@ scroll to', y)
+  openKTalk = () => {
+    KakaoSDK.Channel.chat(channelId).catch(() => {
+      this.props.action.toast.push(Toast.NOT_OPENED);
+    });
+  };
 
-    if (this.scrollView.current)
-      this.scrollView.current.scrollTo({x: 0, y: y, animated: true});
-  }
-
-  _clickTab = idx => () => {
+  clickTab = (idx) => () => {
     // console.log('@@@ click tab', this._webView1)
 
     Analytics.trackEvent('Page_View_Count', {page: tabList[idx]});
 
     const height =
       idx < TAB_IDX_ASK_BY_KAKAO
-        ? (this.state['height' + idx] || 0) + HEADER_IMG_HEIGHT
+        ? (this.state[`height${idx}`] || 0) + HEADER_IMG_HEIGHT
         : 0;
-    this._scrollTo(height);
+    this.scrollTo(height);
     this.setState({tabIdx: idx});
   };
-  _openKTalk = () => {
-    KakaoSDK.Channel.chat(channelId).catch(_ => {
-      this.props.action.toast.push(Toast.NOT_OPENED);
+
+  checkWindowSize(sizeString = '') {
+    const sz = sizeString.split(',');
+    const scale = windowWidth / Number(sz[0]);
+
+    // console.log('@@@ height', sizeString, scale)
+
+    for (let i = 2; i < sz.length; i++) {
+      // 각 tab별로 시작 위치를 설정한다.
+      this.setState({
+        [`height${i - 2}`]: Math.ceil(Number(sz[i]) * scale),
+      });
+    }
+    // 전체 화면의 높이를 저장한다.
+    this.setState({
+      [`height${i - 2}`]: Math.ceil(Number(sz[1]) * scale),
     });
-  };
+  }
+
+  checkIdx(event) {
+    const {contentOffset} = event;
+    let offset = contentOffset.y;
+
+    // console.log('@@@ offset', offset, event)
+
+    offset -= HEADER_IMG_HEIGHT;
+    if (this.state.tabIdx !== TAB_IDX_ASK_BY_KAKAO) {
+      for (let idx = 0; idx < TAB_IDX_ASK_BY_KAKAO; idx++) {
+        // 어떤 tab 위치를 스크롤하고 있는지 계산한다.
+        if (offset < this.state[`height${idx + 1}`]) {
+          if (this.state.tabIdx !== idx) this.setState({tabIdx: idx});
+          break;
+        }
+      }
+    }
+  }
+
+  scrollTo(y) {
+    // console.log('@@@ scroll to', y)
+
+    if (this.scrollView.current)
+      this.scrollView.current.scrollTo({x: 0, y, animated: true});
+  }
 
   renderContactKakao() {
     return (
@@ -212,58 +284,34 @@ class ProductDetailScreen extends Component {
         <Text style={styles.kakaoPlus}>{i18n.t('prodDetail:KakaoPlus')}</Text>
         <AppButton
           iconName="openKakao"
-          onPress={this._openKTalk}
+          onPress={this.openKTalk}
           style={{flex: 1}}
         />
       </View>
     );
   }
 
-  _onMessage(event) {
-    const cmd = JSON.parse(event.nativeEvent.data);
-
-    switch (cmd.key) {
-      case 'dimension':
-        this._checkWindowSize(cmd.value);
-        break;
-      case 'move':
-        if (cmd.value) {
-          var moveTo = cmd.value.split('/');
-          this.props.navigation.navigate('Faq', {
-            key: moveTo[0],
-            num: moveTo[1],
-          });
-        }
-        break;
-      case 'copy':
-        this.props.action.toast.push(Toast.COPY_SUCCESS);
-        break;
-      default:
-        Clipboard.setString(cmd.value);
-    }
-  }
-
   renderWebView() {
-    const {height3} = this.state,
-      localOpDetails =
-        this.props.route.params && this.props.route.params.localOpDetails,
-      detail = _.isEmpty(localOpDetails)
-        ? this.props.product.detail
-        : localOpDetails;
+    const {height3} = this.state;
+    const localOpDetails =
+      this.props.route.params && this.props.route.params.localOpDetails;
+    const detail = _.isEmpty(localOpDetails)
+      ? this.props.product.detail
+      : localOpDetails;
 
     return (
       <WebView
         automaticallyAdjustContentInsets={false}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        scalesPageToFit={true}
-        startInLoadingState={true}
+        javaScriptEnabled
+        domStorageEnabled
+        scalesPageToFit
+        startInLoadingState
         // injectedJavaScript={script}
         decelerationRate="normal"
         // onNavigationStateChange={(navState) => this.onNavigationStateChange(navState)}
-        scrollEnabled={true}
+        scrollEnabled
         // source={{html: body + html + script} }
-        onMessage={this._onMessage}
+        onMessage={this.onMessage}
         source={{html: htmlDetailWithCss(detail, script), baseUrl}}
         style={{height: height3 || 1000}}
       />
@@ -272,7 +320,7 @@ class ProductDetailScreen extends Component {
 
   render() {
     const {tabIdx} = this.state;
-    const {navigation, pending, route} = this.props;
+    const {pending, route} = this.props;
 
     return (
       <SafeAreaView style={styles.screen}>
@@ -283,23 +331,19 @@ class ProductDetailScreen extends Component {
           stickyHeaderIndices={[1]} //탭 버튼 고정
           showsVerticalScrollIndicator={false}
           scrollEventThrottle={100}
-          onScroll={state => {
+          onScroll={(state) => {
             this.checkIdx(state.nativeEvent);
           }}>
-          {
-            <View style={{height: HEADER_IMG_HEIGHT}}>
-              <Image
-                style={{height: HEADER_IMG_HEIGHT}}
-                source={{
-                  uri: API.default.httpImageUrl(
-                    route.params && route.params.img,
-                  ),
-                }}
-              />
-            </View>
-          }
+          <View style={{height: HEADER_IMG_HEIGHT}}>
+            <Image
+              style={{height: HEADER_IMG_HEIGHT}}
+              source={{
+                uri: API.default.httpImageUrl(route.params && route.params.img),
+              }}
+            />
+          </View>
 
-          {/* ScrollView  stickyHeaderIndices로 상단 탭을 고정하기 위해서 View한번 더 사용*/}
+          {/* ScrollView  stickyHeaderIndices로 상단 탭을 고정하기 위해서 View한번 더 사용 */}
           <View style={styles.whiteBackground}>
             <View style={styles.tabView}>
               {tabList.map((elm, idx) => (
@@ -308,15 +352,15 @@ class ProductDetailScreen extends Component {
                   style={styles.whiteBackground}
                   titleStyle={[
                     styles.normal16WarmGrey,
-                    idx == tabIdx ? styles.boldClearBlue : {},
+                    idx === tabIdx ? styles.boldClearBlue : {},
                   ]}
                   title={i18n.t(`prodDetail:${elm}`)}
-                  onPress={this._clickTab(idx)}
+                  onPress={this.clickTab(idx)}
                 />
               ))}
             </View>
           </View>
-          {tabIdx == TAB_IDX_ASK_BY_KAKAO
+          {tabIdx === TAB_IDX_ASK_BY_KAKAO
             ? this.renderContactKakao()
             : this.renderWebView()}
         </ScrollView>
@@ -325,58 +369,12 @@ class ProductDetailScreen extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-  },
-  tabView: {
-    height: 60,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginHorizontal: 20,
-  },
-  whiteBackground: {
-    backgroundColor: colors.white,
-  },
-  normal16WarmGrey: {
-    ...appStyles.normal16Text,
-    color: colors.warmGrey,
-  },
-  boldClearBlue: {
-    color: colors.clearBlue,
-    fontWeight: 'bold',
-  },
-  questionImage: {
-    marginBottom: 14,
-    marginTop: 45,
-  },
-  kakaoImage: {},
-  kakaoContainer: {
-    alignItems: 'center',
-    backgroundColor: colors.whiteTwo,
-    height: 350,
-  },
-  kakaoPlus: {
-    ...appStyles.normal14Text,
-    marginTop: 56,
-    marginBottom: 10,
-    color: colors.warmGrey,
-  },
-});
-
 export default connect(
-  state => ({
+  (state) => ({
     product: state.product.toObject(),
     pending: state.pender.pending[productActions.GET_PROD_DETAIL] || false,
   }),
-  dispatch => ({
+  (dispatch) => ({
     action: {
       product: bindActionCreators(productActions, dispatch),
       toast: bindActionCreators(toastActions, dispatch),
