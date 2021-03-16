50182a562bc9d4d34cdbdc2df1d2c4303e5631ae
Yeonsoo Joung <ysjoung@uangel.com>
2021-03-16 16:16:42 +0900

    firebase warning 수정

--- utils/pushNoti.js
+++ utils/pushNoti.js
@@ -1,7 +1,7 @@
 import Analytics from 'appcenter-analytics';
 import _ from 'underscore';
-import firebase from '@react-native-firebase/app';
-// import messaging from '@react-native-firebase/messaging';
+import messaging from '@react-native-firebase/messaging';
+
 class PushNoti {
   constructor() {
     this.callback = undefined;
@@ -45,15 +45,11 @@ class PushNoti {
     onRegister = ({token}) => {},
     onNotification = (notification) => {},
   }) {
-    const enabled = await firebase.messaging().hasPermission();
-    const notifications = firebase.notifications();
-
+    const enabled = messaging().hasPermission();
     if (enabled) {
-      await firebase.messaging().requestPermission();
+      await messaging().requestPermission();
     }
-
-    firebase
-      .messaging()
+    messaging()
       .getToken()
       .then((token) => {
         if (_.isFunction(onRegister)) {
@@ -62,26 +58,24 @@ class PushNoti {
       });
 
     // App이 foreground상태, push noti를 클릭하는 경우
-    this.notificationListener = notifications.onNotification(
-      this.onNoti('onNotification', onNotification),
-    );
+    // this.notificationListener = notifications.onNotification(
+    //   this.onNoti('onNotification', onNotification),
+    // );
 
     // App이 background상태, push noti를 클릭하는 경우
-    this.notificationOpenedListener = notifications.onNotificationOpened(
+    this.notificationOpenedListener = messaging().onNotificationOpenedApp(
       this.onNoti('onNotificationOpened', onNotification),
     );
 
-    // App이 Killed상태, push noti를 클릭하고 앱을 실행하는 경우
-    this.notificationInitListener = notifications
+    // // App이 Killed상태, push noti를 클릭하고 앱을 실행하는 경우
+    this.notificationInitListener = messaging()
       .getInitialNotification()
       .then(this.onNoti('getInitialNotification', onNotification));
 
-    // foreground 상태에서 data만 받아서 처리 (foreground badge 수 변경 전용)
-    this.onMessage = firebase.messaging().onMessage((message) => {
-      console.log('message', message);
+    // // foreground 상태에서 data만 받아서 처리 (foreground badge 수 변경 전용)
+    this.onMessage = messaging().onMessage((message) => {
       const {badge = 0, notiType, iccid} = message.data;
-      firebase.notifications().setBadge(Number(badge));
-
+      messaging().setBadge(Number(badge));
       // sim 카드 해지 알림이 왓을 때 백그라운드
       if (notiType && iccid) {
         onNotification({data: {notiType, iccid}});
