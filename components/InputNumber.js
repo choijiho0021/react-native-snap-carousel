4219fc236b15eadab5e5125e480bdbdf8d99819c
SOOJEONG1 <soojeongs0408@gmail.com>
2021-04-08 08:53:34 +0900

    장바구니 10개 제한

--- components/InputNumber.js
+++ components/InputNumber.js
@@ -52,8 +52,9 @@ const styles = StyleSheet.create({
 Icon.loadFont();
 export default class InputNumber extends PureComponent {
   render() {
-    const {value, onChange, minValue = 1} = this.props;
+    const {value, onChange, minValue = 1, maxValue = 10} = this.props;
     const min = value <= minValue;
+    const max = value >= maxValue;
 
     return (
       <View style={styles.container}>
@@ -65,9 +66,9 @@ export default class InputNumber extends PureComponent {
         <View style={styles.boxCenter}>
           <Text style={styles.text}>{value}</Text>
         </View>
-        <TouchableOpacity onPress={() => onChange(value + 1)}>
+        <TouchableOpacity onPress={() => onChange(value + 1)} disabled={max}>
           <View style={styles.box}>
-            <Icon name="plus" style={styles.abled} />
+            <Icon name="plus" style={max ? styles.disabled : styles.abled} />
           </View>
         </TouchableOpacity>
       </View>
