a532b6546e4e7cd07615d536ea164a2f3d84ba6a
Yeonsoo Joung <ysjoung@uangel.com>
2021-04-20 15:12:44 +0900

    AppActivityIndicator  -  style 추가

--- components/AppActivityIndicator.js
+++ components/AppActivityIndicator.js
@@ -1,30 +1,34 @@
-import React, { PureComponent } from 'react';
-import {
-  StyleSheet,
-  ActivityIndicator,
-} from 'react-native';
-import Layout from '../constants/Layout'
-import { colors } from '../constants/Colors';
+import React, {PureComponent} from 'react';
+import {StyleSheet, ActivityIndicator} from 'react-native';
+import Layout from '../constants/Layout';
+import {colors} from '../constants/Colors';
 
 const styles = StyleSheet.create({
   indicator: {
-    position: "absolute",
+    position: 'absolute',
     zIndex: 100,
-    alignSelf:'center'
+    alignSelf: 'center',
   },
 });
 
 class AppActivityIndicator extends PureComponent {
   render() {
-    const {visible = true, size="large"} = this.props
-    const width = size == 'large' ? 36 : 20
+    const {visible = true, size = 'large', style = {}} = this.props;
+    const width = size == 'large' ? 36 : 20;
     const position = {
-      left:Layout.window.width/2 - width/2, 
-      top:Layout.window.height/2 - width/2
-    }
+      left: Layout.window.width / 2 - width / 2,
+      top: Layout.window.height / 2 - width / 2,
+    };
 
-    return <ActivityIndicator style={[styles.indicator, position]} size={size} color={colors.clearBlue} animating={visible}/>
+    return (
+      <ActivityIndicator
+        style={[styles.indicator, position, style]}
+        size={size}
+        color={colors.clearBlue}
+        animating={visible}
+      />
+    );
   }
 }
 
-export default AppActivityIndicator
+export default AppActivityIndicator;
