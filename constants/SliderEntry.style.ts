5c6134518ee496636fb45f2c58d991c506f91f7c
Bohun Tak <bhtak@uangel.com>
2021-06-23 11:52:55 +0900

    add type

--- constants/SliderEntry.style.ts
+++ constants/SliderEntry.style.ts
@@ -5,7 +5,7 @@ import {colors} from './Colors';
 const IS_IOS = Platform.OS === 'ios';
 const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
 
-function wp(percentage) {
+function wp(percentage: number) {
   const value = (percentage * viewportWidth) / 100;
   return Math.round(value);
 }
