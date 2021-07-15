5906cba21eee0d00922a90f24b130da021c0e59a
Bohun Tak <bhtak@uangel.com>
2021-07-15 13:39:02 +0900

    prop 변경

--- components/AppUserPic.tsx
+++ components/AppUserPic.tsx
@@ -1,35 +1,33 @@
 import React, {memo} from 'react';
-import {View, Image, TouchableOpacity} from 'react-native';
+import {Image, ImageStyle, Pressable} from 'react-native';
 import {API} from '@/submodules/rokebi-utils';
 import AppIcon from './AppIcon';
 
 type AppUserPicProps = {
-  dimension: {width: number; height: number};
+  style: ImageStyle;
   crop?: boolean;
   url?: string;
   icon?: string;
   onPress?: () => void;
 };
 const AppUserPic: React.FC<AppUserPicProps> = ({
-  dimension,
-  crop = true,
+  style,
   url,
   icon,
   onPress = () => {},
 }) => {
   return (
-    <View style={[dimension, {alignSelf: 'center'}]}>
-      <TouchableOpacity onPress={onPress}>
-        {url ? (
-          <Image
-            source={{uri: API.default.httpImageUrl(url)}}
-            style={[dimension, crop && {borderRadius: dimension.width / 2}]}
-          />
-        ) : (
-          <AppIcon name={icon} />
-        )}
-      </TouchableOpacity>
-    </View>
+    <Pressable style={{alignSelf: 'center'}} onPress={onPress}>
+      {url ? (
+        <Image
+          style={style}
+          source={{uri: API.default.httpImageUrl(url)}}
+          resizeMode="contain"
+        />
+      ) : (
+        <AppIcon name={icon} />
+      )}
+    </Pressable>
   );
 };
 
