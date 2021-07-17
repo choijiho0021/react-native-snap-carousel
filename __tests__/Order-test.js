df316d99a07d6fba6d1837826f890fffa3538c50
Bohun Tak <bhtak@uangel.com>
2021-07-17 12:04:46 +0900

    reformat

--- __tests__/Order-test.js
+++ __tests__/Order-test.js
@@ -1,32 +1,35 @@
-import orderApi from '../utils/api/orderApi'
-import 'isomorphic-fetch'
+import orderApi from '../utils/api/orderApi';
+import 'isomorphic-fetch';
 import userApi from '../utils/api/userApi';
 
 const auth = {
-  user : '12345123451234512345',
-  pass : '1111'
-}
-let userId = ''
+  user: '12345123451234512345',
+  pass: '1111',
+};
+let userId = '';
 
 describe('Order API', () => {
   it.skip(`get delivery address`, () => {
-    return userApi.getByName( auth.user, auth).then(resp => {
-      expect(resp.result).toEqual(0)
-      expect(resp.objects.length).toBeGreaterThan(0)
+    return userApi.getByName(auth.user, auth).then((resp) => {
+      expect(resp.result).toEqual(0);
+      expect(resp.objects.length).toBeGreaterThan(0);
 
-      userId = resp.objects[0].id
+      userId = resp.objects[0].id;
 
-      console.log("user", userId, auth)
+      console.log('user', userId, auth);
 
-      return orderApi.getCustomerProfile(userId, auth).then(resp =>  {
-        console.log('resp', resp)
-        expect(resp.result).toEqual(0)
-        expect(resp.objects.length).toBeGreaterThan(0)
-      }).catch( err => {
-        console.log('failed to get address', err)
-        expect(err).toBeNull()
-      })
-    })
+      return orderApi
+        .getCustomerProfile(userId, auth)
+        .then((resp) => {
+          console.log('resp', resp);
+          expect(resp.result).toEqual(0);
+          expect(resp.objects.length).toBeGreaterThan(0);
+        })
+        .catch((err) => {
+          console.log('failed to get address', err);
+          expect(err).toBeNull();
+        });
+    });
   });
 
   it('add delivery address', () => {
@@ -38,38 +41,45 @@ describe('Order API', () => {
       recipient: '유엔젤',
       roadAddr: '수내동 현대 오피스 10층 (주) 유엔젤 ',
       phone: '0317106222',
-      zipNo: '12345'
-    }
+      zipNo: '12345',
+    };
 
-    return orderApi.addDeliveryAddress(addr, auth).then(resp =>  {
-      console.log('resp', resp)
-      expect(resp.result).toEqual(0)
-      expect(resp.objects.length).toBeGreaterThan(0)
+    return orderApi
+      .addDeliveryAddress(addr, auth)
+      .then((resp) => {
+        console.log('resp', resp);
+        expect(resp.result).toEqual(0);
+        expect(resp.objects.length).toBeGreaterThan(0);
 
-      return orderApi.delDeliveryAddress(resp.objects[0].uuid, auth).then( rsp => {
-        console.log('delete', rsp)
-        expect(rsp.result).toEqual(0)
-      }).catch(err => {
-        console.log('failed to delete address', err)
-        expect(err).toBeNull()
+        return orderApi
+          .delDeliveryAddress(resp.objects[0].uuid, auth)
+          .then((rsp) => {
+            console.log('DELETE', rsp);
+            expect(rsp.result).toEqual(0);
+          })
+          .catch((err) => {
+            console.log('failed to delete address', err);
+            expect(err).toBeNull();
+          });
       })
-
-    }).catch( err => {
-      console.log('failed to add address', err)
-      expect(err).toBeNull()
-    })
-  })
+      .catch((err) => {
+        console.log('failed to add address', err);
+        expect(err).toBeNull();
+      });
+  });
 
   it.skip('delete delivery address', () => {
-    const uuid = 'a61e3f5d-354d-41c9-bf4e-9b9496c0c918'
+    const uuid = 'a61e3f5d-354d-41c9-bf4e-9b9496c0c918';
 
-    return orderApi.delDeliveryAddress(uuid, auth).then( resp => {
-      console.log('delete', resp)
-      expect(resp.result).toEqual(0)
-    }).catch(err => {
-      console.log('failed to delete address', err)
-      expect(err).toBeNull()
-    })
-
-  })
+    return orderApi
+      .delDeliveryAddress(uuid, auth)
+      .then((resp) => {
+        console.log('DELETE', resp);
+        expect(resp.result).toEqual(0);
+      })
+      .catch((err) => {
+        console.log('failed to delete address', err);
+        expect(err).toBeNull();
+      });
+  });
 });
