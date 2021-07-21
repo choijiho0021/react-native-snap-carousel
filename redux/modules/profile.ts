44b43a0611c7f43a35bda92657bc11fcffdc9f25
Bohun Tak <bhtak@uangel.com>
2021-07-21 10:08:06 +0900

    redux toolkit 적용

--- redux/modules/profile.ts
+++ redux/modules/profile.ts
@@ -1,51 +1,32 @@
-import {createAction, handleActions} from 'redux-actions';
-import {pender} from 'redux-pender';
+/* eslint-disable no-param-reassign */
+import {Reducer} from 'redux-actions';
+import {AnyAction} from 'redux';
 import {API} from '@/submodules/rokebi-utils';
-import {createAsyncThunk} from '@reduxjs/toolkit';
-import {AppThunk} from '..';
-
-const UPDATE_PROFILE_ADDRESS = 'rokebi/order/UPDATE_PROFILE_ADDRESS';
-const GET_CUSTOMER_PROFILE = 'rokebi/order/GET_CUSTOMER_PROFILE';
-const ADD_CUSTOMER_PROFILE = 'rokebi/order/ADD_CUSTOMER_PROFILE';
-const UPDATE_CUSTOMER_PROFILE = 'rokebi/order/UPDATE_CUSTOMER_PROFILE';
-const DELETE_CUSTOMER_PROFILE = 'rokebi/order/DELETE_CUSTOMER_PROFILE';
-const SELECTED_ADDR = 'rokebi/order/SELECTED_ADDR';
+import {createAsyncThunk, createSlice, RootState} from '@reduxjs/toolkit';
+import {RkbProfile} from '@/submodules/rokebi-utils/api/profileApi';
 
 // add address list
-export const updateProfileAddress = createAction(UPDATE_PROFILE_ADDRESS);
 export const getCustomerProfile = createAsyncThunk(
-  GET_CUSTOMER_PROFILE,
+  'profile/getCustomer',
   API.Profile.getCustomerProfile,
 );
 export const addCustomerProfile = createAsyncThunk(
-  ADD_CUSTOMER_PROFILE,
+  'profile/addCustomer',
   API.Profile.addCustomerProfile,
 );
 export const updateCustomerProfile = createAsyncThunk(
-  UPDATE_CUSTOMER_PROFILE,
+  'profile/updateCustomer',
   API.Profile.updateCustomerProfile,
 );
 export const delCustomerProfile = createAsyncThunk(
-  DELETE_CUSTOMER_PROFILE,
+  'profile/delCustomer',
   API.Profile.delCustomerProfile,
 );
-export const selectedAddr = createAction(SELECTED_ADDR);
-
-export const actions = {
-  updateProfileAddress,
-  getCustomerProfile,
-  addCustomerProfile,
-  updateCustomerProfile,
-  delCustomerProfile,
-  selectedAddr,
-};
-
-export type ProfileAction = typeof actions;
 
 export interface ProfileModelState {
   // selectedAddrIdx: undefined,
   selectedAddr?: string;
-  profile: object[];
+  profile: RkbProfile[];
   addr: object;
 }
 
@@ -55,161 +36,136 @@ const initialState: ProfileModelState = {
   addr: {},
 };
 
-export const profileDelAndGet = (uuid: string, account): AppThunk => (
-  dispatch,
-  getState,
-) => {
-  const {
-    profile: {profile: pf},
-  } = getState();
-  const deleted = pf.find((item) => item.uuid === uuid);
-  const updateProfile = pf.find((item) => item.uuid !== uuid);
+export const profileDelAndGet = createAsyncThunk(
+  'profile/delAndGet',
+  ({uuid, token}: {uuid: string; token: string}, {dispatch, getState}) => {
+    const {
+      account: {uid},
+      profile: {profile: pf},
+    } = getState() as RootState;
 
-  return dispatch(delCustomerProfile(uuid, account)).then(
-    (resp) => {
-      if (resp.result === 0) {
-        if (deleted && deleted.isBasicAddr && updateProfile) {
-          return dispatch(
-            updateCustomerProfile(
-              {
-                ...updateProfile,
-                isBasicAddr: true,
-              },
-              account,
-            ),
-          ).then((res) => {
-            return dispatch(getCustomerProfile(account));
-          });
+    const deleted = pf.find((item) => item.uuid === uuid);
+    const updateProfile = pf.find((item) => item.uuid !== uuid);
+
+    return dispatch(delCustomerProfile({uuid, token})).then(
+      ({payload: resp}) => {
+        if (resp.result === 0) {
+          if (deleted && deleted.isBasicAddr && updateProfile) {
+            return dispatch(
+              updateCustomerProfile({
+                profile: {...updateProfile, isBasicAddr: true},
+                token,
+              }),
+            ).then(() => dispatch(getCustomerProfile({uid, token})));
+          }
+          return dispatch(getCustomerProfile({uid, token}));
         }
-        return dispatch(getCustomerProfile(account));
-      }
-      throw new Error('Failed to delete Profile');
-    },
-    (err) => {
-      throw err;
-    },
-  );
-};
+        throw new Error('Failed to delete Profile');
+      },
+      (err) => {
+        throw err;
+      },
+    );
+  },
+);
 
-export const profileAddAndGet = (
-  profile,
-  defaultProfile,
-  account,
-): AppThunk => (dispatch) => {
-  return dispatch(addCustomerProfile(profile, defaultProfile, account)).then(
-    (resp) => {
-      return dispatch(getCustomerProfile(account));
-    },
-    (err) => {
-      throw err;
-    },
-  );
-};
+export const profileAddAndGet = createAsyncThunk(
+  'profile/addAndGet',
+  ({profile, token}: {profile: RkbProfile; token: string}, {dispatch}) =>
+    dispatch(addCustomerProfile({profile, token})).then(
+      () => dispatch(getCustomerProfile({token})),
+      (err) => {
+        throw err;
+      },
+    ),
+);
 
-const sortProfile = (a, b) => {
+const sortProfile = (a: RkbProfile, b: RkbProfile) => {
   if (a.isBasicAddr) return -1;
   if (b.isBasicAddr) return 1;
   return a.alias.localeCompare(b.alias);
 };
 
-export default handleActions(
-  {
-    // addr uuid
-    [SELECTED_ADDR]: (state, action) => {
-      return {
-        ...state,
-        selectedAddr: action.payload,
-      };
-    },
-
-    [UPDATE_PROFILE_ADDRESS]: (state, action) => {
-      console.log('update profile address!! action', action);
-      return {
-        ...state,
-        addr: {
-          ...action.payload,
-          // provinceCd : findEngAddress.city[provinceNumber]
-        },
-      };
-    },
-
-    ...pender<ProfileModelState>({
-      type: GET_CUSTOMER_PROFILE,
-      onSuccess: (state, action) => {
-        const {result, objects} = action.payload;
-
-        if (result === 0 && objects.length >= 0) {
-          // const list = state.get('profile')
-          // const idx = list.findIndex(item => item.isBasicAddr)
-          // if(idx>0){
-          //   const tmp = Object.assign({}, list[0])
-          //   list[0] = list[idx]
-          //   list[idx] = tmp
-          // }
-
-          // const list = state.get('profile')
-          // const idx = list.findIndex(item => item.isBasicAddr)
-          // if(idx > 0){
-
-          return {
-            ...state,
-            profile: objects.sort(sortProfile),
-          };
-          // }
-        }
-        return state;
-      },
-    }),
-
-    ...pender<ProfileModelState>({
-      type: ADD_CUSTOMER_PROFILE,
-      onSuccess: (state, action) => {
-        const {result, objects} = action.payload;
-        const {profile} = state;
-
-        if (result === 0 && objects.length > 0) {
-          return {
-            ...state,
-            profile: objects.concat(profile).sort(sortProfile),
-          };
-        }
-        return state;
-      },
-    }),
-
-    ...pender<ProfileModelState>({
-      type: UPDATE_CUSTOMER_PROFILE,
-      onSuccess: (state, action) => {
-        const {result, objects} = action.payload;
-
-        if (result === 0 && objects.length > 0) {
-          console.log('pender update', objects);
-
-          const {profile} = state;
-          const idx = profile.findIndex(
-            (item) => item.uuid === objects[0].uuid,
-          );
-          // TODO : profile[]을 변경하는데, state.update()는 호출이 안됨. 목적이 무엇인지?
-          profile[idx] = objects[0];
-
-          // 이전 기본배송지 profile IDX
-          const prevIdx = profile.findIndex(
-            (item) => item.uuid !== objects[0].uuid && item.isBasicAddr,
-          );
-
-          // 현재 배송지를 기본배송지로 update할 경우, 이전 것 false로 변경
-          if (objects[0].isBasicAddr && prevIdx >= 0) {
-            profile[prevIdx] = {...profile[prevIdx], isBasicAddr: false};
-          }
-
-          return {
-            ...state,
-            profile: profile.sort(sortProfile),
-          };
-        }
-        return state;
-      },
-    }),
-  },
+const slice = createSlice({
+  name: 'profile',
   initialState,
-);
+  reducers: {
+    // addr uuid
+    selectedAddr: (state, action) => {
+      state.selectedAddr = action.payload;
+    },
+    updateProfileAddress: (state, action) => {
+      console.log('update profile address!! action', action);
+      state.addr = action.payload;
+      // provinceCd : findEngAddress.city[provinceNumber]
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCustomerProfile.fulfilled, (state, action) => {
+      const {result, objects} = action.payload;
+
+      if (result === 0 && objects.length >= 0) {
+        // const list = state.get('profile')
+        // const idx = list.findIndex(item => item.isBasicAddr)
+        // if(idx>0){
+        //   const tmp = Object.assign({}, list[0])
+        //   list[0] = list[idx]
+        //   list[idx] = tmp
+        // }
+
+        // const list = state.get('profile')
+        // const idx = list.findIndex(item => item.isBasicAddr)
+        // if(idx > 0){
+
+        state.profile = objects.sort(sortProfile);
+        // }
+      }
+    });
+
+    builder.addCase(addCustomerProfile.fulfilled, (state, action) => {
+      const {result, objects} = action.payload;
+      const {profile} = state;
+
+      if (result === 0 && objects.length > 0) {
+        state.profile = objects.concat(profile).sort(sortProfile);
+      }
+    });
+
+    builder.addCase(updateCustomerProfile.fulfilled, (state, action) => {
+      const {result, objects} = action.payload;
+
+      if (result === 0 && objects.length > 0) {
+        console.log('pender update', objects);
+
+        const {profile} = state;
+        const idx = profile.findIndex((item) => item.uuid === objects[0].uuid);
+        // TODO : profile[]을 변경하는데, state.update()는 호출이 안됨. 목적이 무엇인지?
+        profile[idx] = objects[0];
+
+        // 이전 기본배송지 profile IDX
+        const prevIdx = profile.findIndex(
+          (item) => item.uuid !== objects[0].uuid && item.isBasicAddr,
+        );
+
+        // 현재 배송지를 기본배송지로 update할 경우, 이전 것 false로 변경
+        if (objects[0].isBasicAddr && prevIdx >= 0) {
+          profile[prevIdx] = {...profile[prevIdx], isBasicAddr: false};
+        }
+
+        state.profile = profile.sort(sortProfile);
+      }
+    });
+  },
+});
+
+export const actions = {
+  ...slice.actions,
+  getCustomerProfile,
+  addCustomerProfile,
+  updateCustomerProfile,
+  delCustomerProfile,
+};
+
+export type ProfileAction = typeof actions;
+
+export default slice.reducer as Reducer<ProfileModelState, AnyAction>;
