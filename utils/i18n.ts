b26b178d4baf5b027d86b1f4ab8700c1db9c43cd
Bohun Tak <bhtak@uangel.com>
2021-07-22 17:18:36 +0900

    i18n loading 후 처리할 수 있도록 event module 추가

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,5 +1,8 @@
 import * as RNLocalize from 'react-native-localize';
 import i18n from 'i18n-js';
+import {EventEmitter} from 'events';
+
+export const i18nEvent = new EventEmitter();
 
 export const setI18nConfig = () => {
   const fallback = {languageTag: 'ko'};
@@ -11,12 +14,14 @@ export const setI18nConfig = () => {
     case 'en':
       import('../constants/locale/en.json').then((en) => {
         i18n.translations = {en};
+        i18nEvent.emit('loaded');
       });
       break;
     default:
     case 'ko':
       import('../constants/locale/ko.json').then((ko) => {
         i18n.translations = {ko};
+        i18nEvent.emit('loaded');
       });
       break;
   }
--- utils/validationUtil.ts
+++ utils/validationUtil.ts
@@ -1,6 +1,5 @@
 import validate0 from 'validate.js';
-import _ from 'underscore';
-import i18n from './i18n';
+import i18n, {i18nEvent} from './i18n';
 
 export type ValidationRule = {
   [x: string]: {
@@ -19,128 +18,147 @@ export type ValidationRule = {
   };
 };
 
-const validation: ValidationRule = {
-  password: {
-    presence: {
-      message: '^Please enter a password',
-    },
-    length: {
-      minimum: 5,
-      message: '^Your password must be at least 5 characters',
-    },
-  },
-  email: {
-    presence: {
-      message: i18n.t('reg:noEmail'),
+let validation: ValidationRule = {};
+
+i18nEvent.on('loaded', () => {
+  // do localization related stuff…
+  validation = {
+    password: {
+      presence: {
+        message: '^Please enter a password',
+      },
+      length: {
+        minimum: 5,
+        message: '^Your password must be at least 5 characters',
+      },
     },
     email: {
-      message: i18n.t('reg:invalidEmail'),
+      presence: {
+        message: i18n.t('reg:noEmail'),
+      },
+      email: {
+        message: i18n.t('reg:invalidEmail'),
+      },
     },
-  },
-  actCode: {
-    presence: {
-      message: i18n.t('reg:noActCode'),
+    actCode: {
+      presence: {
+        message: i18n.t('reg:noActCode'),
+      },
+      format: {
+        pattern: /^\d{4}$/g,
+        message: i18n.t('reg:invalidActCode'),
+      },
     },
-    format: {
-      pattern: /^\d{4}$/g,
-      message: i18n.t('reg:invalidActCode'),
+    pin: {
+      presence: {
+        message: i18n.t('reg:noActCode'),
+      },
+      format: {
+        pattern: /^\d{4}$/,
+        message: i18n.t('reg:invalidActCode'),
+      },
     },
-  },
-  pin: {
-    presence: {
-      message: i18n.t('reg:noActCode'),
+    mobile: {
+      presence: {
+        message: i18n.t('reg:noMobile'),
+      },
+      format: {
+        pattern: /^\d{4}-\d{4}$|^\d{3}-\d{4}-\d{4}$/,
+        message: i18n.t('reg:invalidMobile'),
+      },
     },
-    format: {
-      pattern: /^\d{4}$/,
-      message: i18n.t('reg:invalidActCode'),
+    addrName: {
+      presence: {
+        message: i18n.t('purchase:noAddrName'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('purchase:invalidAddrName'),
+      },
     },
-  },
-  mobile: {
-    presence: {
-      message: i18n.t('reg:noMobile'),
+    // profile
+    alias: {
+      // presence: true,
+      presence: {
+        message: i18n.t('addr:noAlias'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('addr:invalidAlias'),
+      },
     },
-    format: {
-      pattern: /^\d{4}-\d{4}$|^\d{3}-\d{4}-\d{4}$/,
-      message: i18n.t('reg:invalidMobile'),
+    recipient: {
+      presence: {
+        message: i18n.t('addr:noRecipient'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('addr:invalidRecipient'),
+      },
     },
-  },
-  addrName: {
-    presence: {
-      message: i18n.t('purchase:noAddrName'),
+    recipientNumber: {
+      presence: {
+        message: i18n.t('addr:noTelephone'),
+      },
+      length: {
+        minimum: 8,
+        message: i18n.t('addr:invalidTelephone'),
+      },
     },
-    length: {
-      minimum: 2,
-      message: i18n.t('purchase:invalidAddrName'),
+    addressLine1: {
+      presence: {
+        message: i18n.t('addr:noAddress'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('addr:invalidAddress'),
+      },
     },
-  },
-  // profile
-  alias: {
-    // presence: true,
-    presence: {
-      message: i18n.t('addr:noAlias'),
+    addressLine2: {
+      presence: {
+        message: i18n.t('addr:noAddress'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('addr:invalidAddress'),
+      },
     },
-    length: {
-      minimum: 2,
-      message: i18n.t('addr:invalidAlias'),
+    detailAddr: {
+      presence: {
+        message: i18n.t('addr:noDetails'),
+      },
+      length: {
+        minimum: 2,
+        message: i18n.t('addr:invalidDetails'),
+      },
     },
-  },
-  recipient: {
-    presence: {
-      message: i18n.t('addr:noRecipient'),
+    mobileSms: {
+      presence: {
+        message: i18n.t('reg:noMobile'),
+      },
+      format: {
+        pattern: /^01(?:0|1|[6-9])\d{8}$/,
+        message: i18n.t('reg:invalidMobile'),
+      },
     },
-    length: {
-      minimum: 2,
-      message: i18n.t('addr:invalidRecipient'),
-    },
-  },
-  recipientNumber: {
-    presence: {
-      message: i18n.t('addr:noTelephone'),
-    },
-    length: {
-      minimum: 8,
-      message: i18n.t('addr:invalidTelephone'),
-    },
-  },
-  addressLine1: {
-    presence: {
-      message: i18n.t('addr:noAddress'),
-    },
-    length: {
-      minimum: 2,
-      message: i18n.t('addr:invalidAddress'),
-    },
-  },
-  addressLine2: {
-    presence: {
-      message: i18n.t('addr:noAddress'),
-    },
-    length: {
-      minimum: 2,
-      message: i18n.t('addr:invalidAddress'),
-    },
-  },
-  detailAddr: {
-    presence: {
-      message: i18n.t('addr:noDetails'),
-    },
-    length: {
-      minimum: 2,
-      message: i18n.t('addr:invalidDetails'),
-    },
-  },
-  mobileSms: {
-    presence: {
-      message: i18n.t('reg:noMobile'),
-    },
-    format: {
-      pattern: /^01(?:0|1|[6-9])\d{8}$/,
-      message: i18n.t('reg:invalidMobile'),
-    },
-  },
-};
+  };
+  console.log('@@@ set validation rule', validation);
+});
 
-export type ValidationKey = keyof typeof validation;
+export type ValidationKey =
+  | 'password'
+  | 'email'
+  | 'actCode'
+  | 'pin'
+  | 'mobile'
+  | 'addrName'
+  | 'alias'
+  | 'recipient'
+  | 'recipientNumber'
+  | 'addressLine1'
+  | 'addressLine2'
+  | 'detailAddr'
+  | 'mobileSms';
 /*
 validate.validators.custom = function(value, options, key, attributes) {
   if ( key == 'actCode' && value != options) return i18n.t('reg:invalidActCode')
