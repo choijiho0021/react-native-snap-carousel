39581c6949b76505d1514d9db310c8f82da576ca
Bohun Tak <bhtak@uangel.com>
2021-07-22 10:05:54 +0900

    require -> import 로 변경

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,20 +1,27 @@
 import * as RNLocalize from 'react-native-localize';
 import i18n from 'i18n-js';
 
-const translationGetters = {
-  en: () => require('../constants/locale/en.json'),
-  ko: () => require('../constants/locale/ko.json'),
-};
-
 export const setI18nConfig = () => {
   const fallback = {languageTag: 'ko'};
   const {languageTag} =
-    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
-    fallback;
+    RNLocalize.findBestAvailableLanguage(['ko', 'en']) || fallback;
 
   // translate.cache.clear();
-  i18n.translations = {[languageTag]: translationGetters[languageTag]()};
+  switch (languageTag) {
+    case 'en':
+      import('../constants/locale/en.json').then((en) => {
+        i18n.translations = {en};
+      });
+      break;
+    default:
+    case 'ko':
+      import('../constants/locale/ko.json').then((ko) => {
+        i18n.translations = {ko};
+      });
+      break;
+  }
   i18n.locale = languageTag;
+  i18n.defaultLocale = languageTag;
 };
 
 export default i18n;
