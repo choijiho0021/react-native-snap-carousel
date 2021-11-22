755a56ec59a51d3696ad7a4113b61e60eebdaaf5
SOOJEONG1 <soojeongs0408@gmail.com>
2021-11-23 08:48:22 +0900

    업데이트 알림 ( 필수 업데이트 제외 )

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import store from '@/store';
-import i18n, {setI18nConfig} from '@/utils/i18n';
 import React, {useEffect} from 'react';
-import {BackHandler, Linking, Platform} from 'react-native';
+import {Linking} from 'react-native';
 import codePush from 'react-native-code-push';
-import RNExitApp from 'react-native-exit-app';
 import VersionCheck from 'react-native-version-check';
 import {Provider} from 'react-redux';
+import i18n, {setI18nConfig} from '@/utils/i18n';
+import store from '@/store';
 import AppAlert from './components/AppAlert';
 import AppComponent from './components/AppComponent';
 
@@ -17,22 +16,6 @@ const App = ({skipLoadingScreen}: {skipLoadingScreen: boolean}) => {
 
     VersionCheck.needUpdate().then(async (res) => {
       if (res.isNeeded) {
-        // if (res.currentVersion < '1.1.8') {
-        //   AppAlert.info(
-        //     i18n.t('noti:updateReq'),
-        //     i18n.t('noti:updateTitleReq'),
-        //     () => {
-        //       // open store if update is needed.
-        //       Linking.openURL(res.storeUrl).then((v) => {
-        //         console.log('@@ v', v);
-        //         if (v) {
-        //           if (Platform.OS === 'ios') RNExitApp.exitApp();
-        //           else BackHandler.exitApp();
-        //         }
-        //       });
-        //     },
-        //   );
-        // } else {
         if (res.currentVersion < res.latestVersion)
           AppAlert.confirm(
             i18n.t('noti:updateTitle'),
@@ -41,7 +24,6 @@ const App = ({skipLoadingScreen}: {skipLoadingScreen: boolean}) => {
               ok: () => Linking.openURL(res.storeUrl),
             },
           );
-        // }
       }
     });
   }, []);
