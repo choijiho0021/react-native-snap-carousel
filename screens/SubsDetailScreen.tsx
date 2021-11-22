0acfe6d44a755ff3d900b6ea43db05e967d8b7c9
Yeonsoo Joung <ysjoung@uangel.com>
2021-11-22 15:48:35 +0900

    사용안하는 import 제거, import순서 변경(warning 제거)

--- screens/SubsDetailScreen.tsx
+++ screens/SubsDetailScreen.tsx
@@ -1,3 +1,7 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import AppActivityIndicator from '@/components/AppActivityIndicator';
 import AppBackButton from '@/components/AppBackButton';
 import AppButton from '@/components/AppButton';
@@ -13,11 +17,6 @@ import {actions as accountActions} from '@/redux/modules/account';
 import {actions as orderActions} from '@/redux/modules/order';
 import {actions as productActions} from '@/redux/modules/product';
 import i18n from '@/utils/i18n';
-import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {ApiToken} from '../redux/api/api';
 
 const activateBtn = 'activateBtn';
 const deactivateBtn = 'deactivateBtn';
