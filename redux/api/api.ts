f9f3d24e72dc04898b16471b3d6df579c3afb9c6
SOOJEONG1 <soojeongs0408@gmail.com>
2022-01-29 15:40:15 +0900

    production일 경우, port제거

--- redux/api/api.ts
+++ redux/api/api.ts
@@ -6,7 +6,7 @@ import i18n from '@/utils/i18n';
 import Env from '@/environment';
 
 export type Langcode = 'ko' | 'en';
-const {scheme, apiUrl, appId, esimGlobal, rokApiUrl} = Env.get();
+const {scheme, apiUrl, appId, esimGlobal, rokApiUrl, isProduction} = Env.get();
 
 const FAILED = -1000;
 const E_NOT_FOUND = -1001;
@@ -129,9 +129,9 @@ const addrApiUrl = () => {
 };
 
 const rokHttpUrl = (path0: string, port?: number) => {
-  return `${scheme}://${rokApiUrl}${port ? `:${port}` : ''}/${path0}?service=${
-    esimGlobal ? 'global' : appId
-  }`;
+  return `${scheme}://${rokApiUrl}${
+    isProduction ? '' : `:${port || 80}`
+  }/${path0}?service=${esimGlobal ? 'global' : appId}`;
 };
 
 const queryString = (obj: Record<string, string | number>) => {
