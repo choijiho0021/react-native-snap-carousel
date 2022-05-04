eebbfac140f946dcc1690317ad9bec5efe1d5e0b
Yeonsoo Joung <ysjoung@uangel.com>
2022-05-04 14:43:59 +0900

    IOS 실행가능하도록 수정

--- ios/Bridge-Header.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Bridge-Header.swift
-//  RokebiESIM
-//
-//  Created by 정연수 on 2021/11/10.
-//  Copyright © 2021 Facebook. All rights reserved.
-//
-
-import Foundation
---
+++ ios/Rokebi-Bridge.swift
@@ -0,0 +1,9 @@
+//
+//  Rokebi-Bridge.swift
+//  RokebiESIM
+//
+//  Created by 정연수 on 2022/05/04.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
