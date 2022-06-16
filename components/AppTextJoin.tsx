ff50f57e9818dd85018cac1f2d53200197de2355
Yeonsoo Joung <ysjoung@uangel.com>
2022-06-16 13:48:31 +0900

    AppTextJoin 밑줄을 위한 수정, 사용 예시 데이터 추가

--- components/AppTextJoin.tsx
+++ components/AppTextJoin.tsx
@@ -4,31 +4,58 @@ import AppText from './AppText';
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection:'row',
+    flexDirection: 'row',
   },
-  text:{
-    alignSelf:'flex-end'
-  }
+  text: {
+    alignSelf: 'flex-end',
+  },
 });
 
-type StyledText = {
-  text:string,
-  style:TextStyle,
+export type StyledText = {
+  text: string;
+  textStyle: TextStyle;
+  viewStyle: ViewStyle;
 };
 
 const AppTextJoin = ({
   data,
   style,
 }: {
-  data:StyledText[],
+  data: StyledText[];
   style?: ViewStyle;
 }) => {
   return (
-    <View style={[style || styles.container]}>
-      {data.map(elm => (
-        <AppText style={[styles.text, elm.style]}>{elm.text} </AppText>
+    <View style={[styles.container, style]}>
+      {data.map((elm) => (
+        <View style={elm.viewStyle}>
+          <AppText style={[styles.text, elm.textStyle]}>{elm.text} </AppText>
+        </View>
       ))}
     </View>
   );
 };
 export default memo(AppTextJoin);
+
+// 사용예시
+// const data: StyledText[] = [
+//   {
+//     text: 'QR코드',
+//     viewStyle: {
+//       borderStyle: 'solid',
+//       borderBottomWidth: 10,
+//       borderBottomColor: '#b8d1f5',
+//     },
+//     textStyle: {
+//       ...appStyles.bold24Text,
+//       top: 10,
+//     },
+//   },
+//   {
+//     text: '혹은',
+//     viewStyle: {},
+//     textStyle: {
+//       ...appStyles.bold20Text,
+//       top: 10,
+//     },
+//   },
+// ];
