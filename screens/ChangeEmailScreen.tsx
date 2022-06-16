6a5dd0a45564476d8bbcf09d7ba415154be34676
Yeonsoo Joung <ysjoung@uangel.com>
2022-06-16 14:43:08 +0900

    이메일 중복확인 및 디자인 변경

--- screens/ChangeEmailScreen.tsx
+++ screens/ChangeEmailScreen.tsx
@@ -1,6 +1,6 @@
 import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import React, {Component} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {StyleSheet, View, SafeAreaView} from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -14,14 +14,12 @@ import {
   AccountAction,
   actions as accountActions,
 } from '@/redux/modules/account';
-import {actions as cartActions, CartAction} from '@/redux/modules/cart';
-import {actions as notiActions, NotiAction} from '@/redux/modules/noti';
-import {actions as orderActions, OrderAction} from '@/redux/modules/order';
 import i18n from '@/utils/i18n';
-import AppTextInput from '@/components/AppTextInput';
 import AppButton from '@/components/AppButton';
 import validationUtil, {ValidationResult} from '@/utils/validationUtil';
 import AppModal from '@/components/AppModal';
+import {API} from '@/redux/api';
+import InputEmail, {InputEmailRef} from '@/components/InputEmail';
 
 const styles = StyleSheet.create({
   title: {
@@ -63,6 +61,26 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     marginTop: 10,
   },
+  oldEmail: {
+    height: 46,
+    marginTop: 4,
+    marginBottom: 32,
+    backgroundColor: colors.whiteTwo,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+  },
+  helpText: {
+    ...appStyles.normal14Text,
+    color: colors.clearBlue,
+    marginTop: 13,
+    marginLeft: 30,
+  },
+  divider: {
+    marginTop: 30,
+    width: '100%',
+    height: 10,
+    backgroundColor: '#f5f5f5',
+  },
   button: (isValid) => ({
     ...appStyles.normal16Text,
     height: 52,
@@ -89,146 +107,106 @@ type ChangeEmailScreenRouteProp = RouteProp<HomeStackParamList, 'SimpleText'>;
 type ChangeEmailScreenProps = {
   navigation: ChangeEmailScreenNavigationProp;
   route: ChangeEmailScreenRouteProp;
-
-  isPushNotiEnabled?: boolean;
-  loggedIn?: boolean;
   email?: string;
 
-  pending: boolean;
-  action: {
-    cart: CartAction;
-    order: OrderAction;
+  actions: {
     account: AccountAction;
-    noti: NotiAction;
   };
 };
 
-type ChangeEmailScreenState = {
-  newEmail: string;
-  showModal: boolean;
-  inValid?: ValidationResult;
-};
+const ChangeEmailScreen: React.FC<ChangeEmailScreenProps> = ({
+  navigation,
+  actions,
+  email,
+}) => {
+  const [newEmail, setNewEmail] = useState<string>('');
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [inValid, setInValid] = useState<ValidationResult>({});
+  const emailRef = useRef<InputEmailRef>(null);
 
-class ChangeEmailScreen extends Component<
-  ChangeEmailScreenProps,
-  ChangeEmailScreenState
-> {
-  constructor(props: ChangeEmailScreenProps) {
-    super(props);
-
-    this.state = {
-      newEmail: '',
-      showModal: false,
-    };
-    this.onChangeText = this.onChangeText.bind(this);
-    this.changeEmail = this.changeEmail.bind(this);
-  }
-
-  componentDidMount = async () => {
-    this.props.navigation.setOptions({
+  useEffect(() => {
+    navigation.setOptions({
       title: null,
       headerLeft: () => <AppBackButton title={i18n.t('set:changeMail')} />,
     });
-  };
+  }, [navigation]);
 
-  componentDidUpdate() {}
+  const onChangeText = useCallback((v: string) => {
+    setNewEmail(v);
+    setInValid(validationUtil.validate('email', v));
+  }, []);
 
-  onChangeText = (v: string) => {
-    this.setState({
-      newEmail: v,
-      inValid: validationUtil.validate('email', v),
-    });
-  };
+  const changeEmail = useCallback(async () => {
+    const checkEmail = await API.User.confirmEmail({email: newEmail});
 
-  changeEmail() {
-    const {newEmail} = this.state;
-    const {email} = this.props;
-
-    if (newEmail && email !== newEmail) {
-      this.props.action.account.changeEmail(newEmail).then((resp) => {
-        if (resp.payload.result === 0) {
-          this.setState({showModal: true});
+    if (checkEmail.result === 0) {
+      await actions.account.changeEmail(newEmail).then((rsp) => {
+        if (rsp.payload.result === 0) {
+          setShowModal(true);
         }
       });
+    } else if (checkEmail.message?.includes('Duplicate')) {
+      setInValid({email: [i18n.t('changeEmail:duplicate')]});
+    } else {
+      // dulicated email 이외의 에러인 경우, throw error
+      console.log('confirm email failed', checkEmail);
+      setInValid({email: [i18n.t('changeEmail:fail')]});
+      throw new Error('failed to confirm email');
     }
-  }
+  }, [actions.account, newEmail]);
 
-  render() {
-    const {navigation} = this.props;
-    const {newEmail, inValid, showModal} = this.state;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{padding: 20, flex: 1}}>
+        <AppText style={appStyles.normal14Text}>
+          {i18n.t('changeEmail:using')}
+        </AppText>
+        <AppText style={styles.oldEmail}>{email}</AppText>
+        <AppText style={appStyles.normal14Text}>
+          {i18n.t('changeEmail:new')}
+        </AppText>
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={{padding: 20, flex: 1}}>
-          <AppText style={appStyles.normal14Text}>
-            {i18n.t('changeEmail:using')}
-          </AppText>
-          <AppText
+        <View>
+          <InputEmail
             style={{
-              height: 46,
-              marginTop: 4,
-              marginBottom: 32,
-              backgroundColor: colors.whiteTwo,
-              paddingHorizontal: 15,
-              paddingVertical: 12,
-            }}>
-            {this.props.email}
+              marginTop: 10,
+            }}
+            inputRef={emailRef}
+            value={newEmail}
+            onChange={onChangeText}
+          />
+
+          <AppText style={[styles.helpText, {color: colors.errorBackground}]}>
+            {inValid && inValid.email?.length > 0 ? inValid.email[0] : null}
           </AppText>
-          <AppText style={appStyles.normal14Text}>
-            {i18n.t('changeEmail:new')}
-          </AppText>
-          <View style={styles.inputBox}>
-            <AppTextInput
-              style={styles.textInput}
-              returnKeyType="done"
-              enablesReturnKeyAutomatically
-              onChangeText={this.onChangeText}
-              maxLength={25}
-              placeholder={i18n.t('changeEmail:placeholder')}
-              value={newEmail}
-            />
-            <AppButton
-              style={styles.cancelButton}
-              iconName="btnCancel"
-              onPress={() => this.onChangeText('')}
-            />
-          </View>
-          <AppText style={styles.label}>{i18n.t('changeEmail:info')}</AppText>
-          {inValid && (
-            <AppText style={styles.label}>{inValid.email[0]}</AppText>
-          )}
+          {/* <View key="divider" style={styles.divider} /> */}
         </View>
-        <AppButton
-          style={styles.button(!inValid)}
-          titleStyle={styles.buttonTitle(!inValid)}
-          disabled={!!inValid}
-          title={i18n.t('changeEmail:save')}
-          onPress={this.changeEmail}
-        />
-        <AppModal
-          title={i18n.t('changeEmail:saveInfo')}
-          type="info"
-          onOkClose={() => navigation.goBack()}
-          visible={showModal}
-        />
-      </SafeAreaView>
-    );
-  }
-}
+      </View>
+      <AppButton
+        style={styles.button(!inValid)}
+        titleStyle={styles.buttonTitle(!inValid)}
+        disabled={!!inValid}
+        title={i18n.t('changeEmail:save')}
+        onPress={changeEmail}
+      />
+      <AppModal
+        title={i18n.t('changeEmail:saveInfo')}
+        type="info"
+        onOkClose={() => navigation.goBack()}
+        visible={showModal}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default connect(
-  ({account, status}: RootState) => ({
-    loggedIn: account.loggedIn,
+  ({account}: RootState) => ({
+    account,
     email: account.email,
-    isPushNotiEnabled: account.isPushNotiEnabled,
-    pending: status.pending[accountActions.logout.typePrefix] || false,
   }),
   (dispatch) => ({
-    action: {
+    actions: {
       account: bindActionCreators(accountActions, dispatch),
-      cart: bindActionCreators(cartActions, dispatch),
-      order: bindActionCreators(orderActions, dispatch),
-      noti: bindActionCreators(notiActions, dispatch),
     },
   }),
 )(ChangeEmailScreen);
