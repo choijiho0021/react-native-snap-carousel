e5de4e971ff77f841b5406fbcedad016e02f0aa7
Yeonsoo Joung <ysjoung@uangel.com>
2022-06-23 15:56:01 +0900

    conflict 실수 해결

--- screens/HomeScreen/Esim.tsx
+++ screens/HomeScreen/Esim.tsx
@@ -178,13 +178,7 @@ const Esim: React.FC<EsimProps> = ({
   sync,
 }) => {
   const [isSupportDev, setIsSupportDev] = useState<boolean>(true);
-<<<<<<< HEAD
   const [isDevModalVisible, setIsDevModalVisible] = useState<boolean>(false);
-=======
-  const [isDevModalVisible, setIsDevModalVisible] = useState<
-    boolean | undefined
-  >();
->>>>>>> e5ab75f6b33ba0411600ba18779a20bab602e4d6
   const [index, setIndex] = useState(0);
   const routes = useMemo(
     () =>
@@ -604,7 +598,6 @@ const Esim: React.FC<EsimProps> = ({
         renderTabBar={() => null}
       />
 
-<<<<<<< HEAD
       {isDevModalVisible && !isSupportDev ? (
         <AppModal
           title={i18n.t('home:unsupportedTitle')}
@@ -624,18 +617,6 @@ const Esim: React.FC<EsimProps> = ({
           onCancelClose={() => setPopUpVisible(false)}
         />
       )}
-=======
-      <AppModal
-        title={i18n.t('home:unsupportedTitle')}
-        closeButtonTitle={i18n.t('ok')}
-        titleStyle={styles.modalTitle}
-        type="close"
-        onOkClose={() => exitApp('exit')}
-        visible={isDevModalVisible === true}>
-        {modalBody()}
-      </AppModal>
-
->>>>>>> e5ab75f6b33ba0411600ba18779a20bab602e4d6
       <AppVerModal
         visible={isDevModalVisible === false && appUpdateVisible}
         option={appUpdate}
