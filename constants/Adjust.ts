55ec720fb58fce006653752bdd68ebecf4596a73
Yeonsoo Joung <ysjoung@uangel.com>
2022-07-29 10:32:34 +0900

    adjust 이벤트 정리

--- constants/Adjust.ts
+++ constants/Adjust.ts
@@ -1,11 +1,13 @@
 export const eventToken = {
-  Add_Cart: 'c5v4y0',
-  Apple_Login: 'l6e209',
-  But_Now: '6i1yvf',
-  Cart_Page_View: 'j3qg2q',
-  Country_Selected: 'yydszb',
-  Home_Banner: '4cd1t4',
-  Kakao_Login: '24xzw0',
-  Search_Country: 'wdlinm',
-  SMS_Confirm: 'ow53q9',
+  Login: 'ow53q9',
+  SignUp: 'wdlinm',
+  Sales: '6i1yvf',
+
+  // 숨김 이벤트 - 비활성화
+  // Add_Cart: 'c5v4y0',
+  // Apple_Login: 'l6e209',
+  // Cart_Page_View: 'j3qg2q',
+  // Country_Selected: 'yydszb',
+  // Home_Banner: '4cd1t4',
+  // Kakao_Login: '24xzw0',
 };
