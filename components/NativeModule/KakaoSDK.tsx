50e5022c12c8ce54b2264cc929d542a220827b40
Yeonsoo Joung <ysjoung@uangel.com>
2022-08-02 12:07:46 +0900

    android KakaoSDK (login, sharelink) 통합

--- components/NativeModule/KakaoSDK.tsx
+++ components/NativeModule/KakaoSDK.tsx
@@ -1,5 +1,5 @@
 import {NativeModules} from 'react-native';
-const {KakaoShareLink} = NativeModules;
+const {KakaoShareLink, RNKakaoLogins} = NativeModules;
 
 /**
  * ContentType
@@ -356,6 +356,54 @@ export const sendCustom = (
     });
 };
 
+export declare type KakaoOAuthToken = {
+  accessToken: string;
+  refreshToken: string;
+  idToken: string;
+  accessTokenExpiresAt: Date;
+  refreshTokenExpiresAt: Date;
+  scopes: string[];
+};
+export declare type KakaoAccessTokenInfo = {
+  accessToken: string;
+  expiresIn: string;
+};
+export declare type KakaoProfile = {
+  id: string;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  thumbnailImageUrl: string;
+  phoneNumber: string;
+  ageRange: string;
+  birthday: string;
+  birthdayType: string;
+  birthyear: string;
+  gender: string;
+  isEmailValid: boolean;
+  isEmailVerified: boolean;
+  isKorean: boolean;
+  ageRangeNeedsAgreement?: boolean;
+  birthdayNeedsAgreement?: boolean;
+  birthyearNeedsAgreement?: boolean;
+  emailNeedsAgreement?: boolean;
+  genderNeedsAgreement?: boolean;
+  isKoreanNeedsAgreement?: boolean;
+  phoneNumberNeedsAgreement?: boolean;
+  profileNeedsAgreement?: boolean;
+};
+export declare type KakaoProfileNoneAgreement = {
+  id: string;
+};
+export declare const login: () => Promise<KakaoOAuthToken>;
+export declare const loginWithKakaoAccount: () => Promise<KakaoOAuthToken>;
+export declare const logout: () => Promise<string>;
+export declare const unlink: () => Promise<string>;
+export declare const getProfile: () => Promise<
+  KakaoProfile | KakaoProfileNoneAgreement
+>;
+export declare const getAccessToken: () => Promise<KakaoAccessTokenInfo>;
+
 export declare type KakaoShareLinkType = {
   sendCommerce: typeof sendCommerce;
   sendList: typeof sendList;
@@ -365,4 +413,22 @@ export declare type KakaoShareLinkType = {
   sendCustom: typeof sendCustom;
 };
 
-export default KakaoShareLink as KakaoShareLinkType;
+export declare type RNKakaoLoginsType = {
+  login: typeof login;
+  loginWithKakaoAccount: typeof loginWithKakaoAccount;
+  logout: typeof logout;
+  unlink: typeof unlink;
+  getProfile: typeof getProfile;
+  getAccessToken: typeof getAccessToken;
+};
+
+const KakaoSDK: {
+  KakaoShareLink: KakaoShareLinkType;
+  RNKakaoLogins: RNKakaoLoginsType;
+} = {
+  KakaoShareLink,
+  RNKakaoLogins,
+};
+
+export default KakaoSDK;
+// export default KakaoShareLink as KakaoShareLinkType;
