552831df5c3e27a822b876dbbb3b2253b60d118c
Yeonsoo Joung <ysjoung@uangel.com>
2022-08-12 14:11:17 +0900

    flipper로 디버깅하기 위해 추가

--- store.ts
+++ store.ts
@@ -1,10 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import reducer from '@/redux/index';
 import ReduxThunk from 'redux-thunk';
+const createDebugger = require('redux-flipper').default; // <-- ADD THIS
 
 const store = configureStore({
   reducer,
-  middleware: [ReduxThunk] as const,
+  middleware: [ReduxThunk].concat(createDebugger()),
 });
 export type AppDispatch = typeof store.dispatch;
 export default store;
