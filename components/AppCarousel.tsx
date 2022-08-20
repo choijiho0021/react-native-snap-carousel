433090b4f6832ee87142259e162305dfe1e97a50
bhtak <bhtak@uangel.com>
2022-08-20 11:35:15 +0900

    autoplay 처리

--- components/AppCarousel.tsx
+++ components/AppCarousel.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {useInterval} from '@/utils/useInterval';
 
@@ -26,40 +26,50 @@ const AppCarousel: React.FC<AppCarouselProps<T>> = ({
   autoplay = false,
   loop = false,
 }) => {
-  const [list, setList] = useState(data.slice(0, 3));
+  const slides = useMemo(
+    () => (loop && data.length === 1 ? data.concat(data) : data),
+    [data, loop],
+  );
+  const [list, setList] = useState<T>([]);
   const [idx, setIdx] = useState(loop ? 1 : 0);
   const ref = useRef<ScrollView>();
 
   useEffect(() => {
-    if (loop || (idx > 0 && idx < data.length - 1))
+    if (list.length === 0) {
+      setList(
+        slides.length === 2 ? slides.concat(slides[0]) : slides.slice(0, 3),
+      );
+    }
+  }, [list.length, slides]);
+
+  useEffect(() => {
+    if (loop || (idx > 0 && idx < slides.length - 1))
       ref.current?.scrollTo({x: sliderWidth, y: 0, animated: false});
-  }, [sliderWidth, idx, data.length, loop]);
+  }, [sliderWidth, idx, slides.length, loop]);
 
   const moveSlide = useCallback(
     (newIdx: number) => {
       if (newIdx <= 0) {
         // left most
-        if (loop)
+        if (loop && slides.length > 1)
           setList(
-            data.slice(data.length - 1).concat(data.slice(0, newIdx + 2)),
+            slides.slice(slides.length - 1).concat(slides.slice(0, newIdx + 2)),
           );
-      } else if (newIdx + 2 > data.length) {
+      } else if (newIdx + 2 > slides.length) {
         // right most
-        if (loop) setList(data.slice(newIdx - 1).concat(data[0]));
-      } else {
-        setList(data.slice(newIdx - 1, newIdx + 2));
+        if (loop && slides.length > 1)
+          setList(slides.slice(newIdx - 1).concat(slides[0]));
+      } else if (slides.length > 1) {
+        setList(slides.slice(newIdx - 1, newIdx + 2));
       }
       setIdx(newIdx);
       onSnapToItem(newIdx);
     },
-    [data, loop, onSnapToItem],
+    [slides, loop, onSnapToItem],
   );
 
   useInterval(
-    () => {
-      ref.current?.scrollToEnd({animated: true});
-      moveSlide((idx + 1) % data.length);
-    },
+    () => ref.current?.scrollToEnd({animated: true}),
     autoplay && loop ? 2500 : null,
   );
 
@@ -77,19 +87,17 @@ const AppCarousel: React.FC<AppCarouselProps<T>> = ({
         const i = Math.floor(x / sliderWidth);
         let newIdx = idx;
         if (loop) {
-          if (i > 1) newIdx = (idx + 1) % data.length;
-          else if (i < 1) newIdx = (idx - 1 + data.length) % data.length;
+          if (i > 1) newIdx = (idx + 1) % slides.length;
+          else if (i < 1) newIdx = (idx - 1 + slides.length) % slides.length;
         } else {
           // eslint-disable-next-line no-nested-ternary, no-lonely-if
           if (i === 1)
-            newIdx = idx + (idx === 0 ? 1 : idx === data.length - 1 ? -1 : 0);
-          else if (i > 1 && idx < data.length - 1) newIdx++;
+            newIdx = idx + (idx === 0 ? 1 : idx === slides.length - 1 ? -1 : 0);
+          else if (i > 1 && idx < slides.length - 1) newIdx++;
           else if (i < 1 && idx > 0) newIdx--;
         }
 
-        if (newIdx !== idx) {
-          moveSlide(newIdx);
-        }
+        if (newIdx !== idx) moveSlide(newIdx);
       }}
       snapToAlignment="center">
       <View style={styles.container}>
