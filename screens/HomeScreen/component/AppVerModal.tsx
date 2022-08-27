dd0d2b2a173f07644ed0c3e18bd3cf9346ae88df
bhtak <bhtak@uangel.com>
2022-08-27 13:40:45 +0900

    modal size 조정

--- screens/HomeScreen/component/AppVerModal.tsx
+++ screens/HomeScreen/component/AppVerModal.tsx
@@ -9,6 +9,7 @@ import AppText from '@/components/AppText';
 import Env from '@/environment';
 import AppTextJoin from '@/components/AppTextJoin';
 import AppIcon from '@/components/AppIcon';
+import {MAX_WIDTH} from '@/constants/SliderEntry.style';
 
 const {appStoreUrl} = Env.get();
 
@@ -110,6 +111,8 @@ const AppVerModal: React.FC<AppVerModalProps> = ({
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
         backgroundColor: 'white',
+        maxWidth: MAX_WIDTH,
+        width: '100%',
       }}
       bottom={renderBottom}
       visible={visible}>
