d0283cebfd1138eaf20b50afabaf299860d0be18
Yeonsoo Joung <ysjoung@uangel.com>
2022-09-16 15:54:40 +0900

    android 애플로그인 제거

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   dependencies: {
+    '@invertase/react-native-apple-authentication': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
     '@unimodules/react-native-adapter': {
       platforms: {
         android: null, // disable Android platform, other platforms will still autolink if provided
-
       },
     },
   },
