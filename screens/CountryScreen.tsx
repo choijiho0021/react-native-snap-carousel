ae1eecc3ed679a456f753162997d20f63dfd8756
leeyounsoek05 <>
2022-11-08 15:47:37 +0900

    디자인 QA 12번. 스토어리스트 화면 반영

--- screens/CountryScreen.tsx
+++ screens/CountryScreen.tsx
@@ -213,7 +213,8 @@ const CountryScreen: React.FC<CountryScreenProps> = (props) => {
             ) : null
           }
           renderSectionFooter={({section: {title, data}}) =>
-            title === 'daily' && prodData[1].data.length >= 1 ? (
+            prodData[0].data.length < 0 ||
+            (title === 'daily' && prodData[1].data.length > 0) ? (
               <View style={styles.divider} />
             ) : (
               <View style={{width: '100%', height: 20}} />
