e3f55faac5c74916ed0a2867c5af8abdf2c1d38a
leeyounsoek05 <>
2022-11-08 16:03:15 +0900

    eSIM 구매내역 제목 padding 복구

--- screens/EsimScreen/components/EsimSubs.tsx
+++ screens/EsimScreen/components/EsimSubs.tsx
@@ -91,6 +91,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingVertical: 10,
   },
   inactiveContainer: {
     marginBottom: 6,
