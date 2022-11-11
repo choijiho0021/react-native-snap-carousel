3da05e8866c3158773eef8ecbc31a16c84d7a763
leeyounsoek05 <>
2022-11-11 16:45:45 +0900

    country 페이지에서 데이터 종량제만 있는 경우 구분선 넣지 않게끔 처리

--- screens/CountryScreen.tsx
+++ screens/CountryScreen.tsx
@@ -213,11 +213,14 @@ const CountryScreen: React.FC<CountryScreenProps> = (props) => {
             ) : null
           }
           renderSectionFooter={({section: {title, data}}) =>
-            prodData[0].data.length < 0 ||
-            (title === 'daily' && prodData[1].data.length > 0) ? (
+            title === 'daily' &&
+            prodData[1].data.length > 0 &&
+            prodData[0].data.length > 0 ? (
               <View style={styles.divider} />
             ) : (
-              <View style={{width: '100%', height: 20}} />
+              prodData[0].data.length > 0 && (
+                <View style={{width: '100%', height: 20}} />
+              )
             )
           }
         />
