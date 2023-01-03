76b4a0fe511703a96abdf17f1c48d0f435e2c5f5
Yeonsoo Joung <ysjoung@uangel.com>
2023-01-03 12:31:04 +0900

    iccid 확인 로직 제거

--- screens/ProductDetailScreen/index.tsx
+++ screens/ProductDetailScreen/index.tsx
@@ -329,7 +329,7 @@ const ProductDetailScreen: React.FC<ProductDetailScreenProps> = ({
         textMessage={showSnackBar.text}
         bottom={10}
       />
-      {account.iccid || (esimApp && account.loggedIn) ? (
+      {account.loggedIn ? (
         <View style={styles.buttonBox}>
           <AppButton
             style={styles.btnCart}
