e74073dcb26b4ae0109980f45b5cc2d1721cc85d
bhtak <bhtak@uangel.com>
2023-01-25 14:36:02 +0900

    react-native rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,11 +20,14 @@ if (
      * See the project section above for more information.projecttsconfig.json
      */
     createDefaultProgram: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
   };
 }
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'prettier', 'react-native'],
   // plugins: ['eslint-comments', 'jest', 'unicorn', 'react-hooks'],
   extends: ['airbnb', 'airbnb-typescript', 'prettier'],
   env: {
@@ -34,6 +37,7 @@ module.exports = {
     mocha: true,
     jest: true,
     jasmine: true,
+    'react-native/react-native': true,
   },
   rules: {
     'no-use-before-define': 0,
@@ -132,6 +136,13 @@ module.exports = {
     'react/jsx-filename-extension': [1, {extensions: ['.tsx', '.jsx']}],
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     */
+
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    // 'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
   },
   settings: {
     'import/resolver': {
@@ -139,6 +150,11 @@ module.exports = {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
+    'react-native/style-sheet-object-names': [
+      'EStyleSheet',
+      'OtherStyleSheet',
+      'PStyleSheet',
+    ],
   },
   parserOptions,
 };
