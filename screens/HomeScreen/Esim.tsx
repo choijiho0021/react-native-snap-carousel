9918b9ccc780b31c6fac958331abdc25aec114dd
bhtak <bhtak@uangel.com>
2023-03-25 10:30:46 +0900

    component 분리

--- screens/HomeScreen/Esim.tsx
+++ screens/HomeScreen/Esim.tsx
@@ -34,7 +34,7 @@ import ShortcutBadge from 'react-native-app-badge';
 import {RouteProp, useFocusEffect} from '@react-navigation/native';
 import VersionCheck from 'react-native-version-check';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {isDeviceSize, isFolderOpen} from '@/constants/SliderEntry.style';
+import {isFolderOpen} from '@/constants/SliderEntry.style';
 import AppButton from '@/components/AppButton';
 import AppModal from '@/components/AppModal';
 import AppText from '@/components/AppText';
@@ -76,6 +76,7 @@ import AppVerModal from './component/AppVerModal';
 import RCTNetworkInfo from '@/components/NativeModule/NetworkInfo';
 import AppStyledText from '@/components/AppStyledText';
 import {retrieveData, storeData} from '@/utils/utils';
+import LocalModal from './component/LocalModal';
 
 const {esimGlobal, isIOS} = Env.get();
 
@@ -83,10 +84,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
   },
   title: {
     ...appStyles.title,
@@ -172,111 +169,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderBottomWidth: 2,
   },
-  okBtnContainer: {
-    backgroundColor: colors.white,
-    marginBottom: 16,
-    marginTop: 12,
-  },
-  okButton: {
-    ...appStyles.normal16Text,
-    height: 52,
-    backgroundColor: colors.clearBlue,
-    textAlign: 'center',
-    color: '#ffffff',
-  },
-  localModalTitle: {
-    marginBottom: 24,
-  },
-  localModalTitleText: {
-    ...appStyles.bold24Text,
-    lineHeight: 34,
-  },
-  localModalBody: {
-    padding: 20,
-    backgroundColor: colors.backGrey,
-    marginBottom: 12,
-    flexDirection: 'row',
-    borderRadius: 3,
-  },
-  localModalBodyIcon: {
-    marginRight: 6,
-    marginTop: 2,
-  },
-  localModalBodyTitle: {
-    ...appStyles.bold20Text,
-    color: colors.clearBlue,
-    lineHeight: 24,
-    marginBottom: 4,
-  },
-  localModalBodyText: {
-    ...appStyles.medium16,
-    fontSize: isDeviceSize('medium', true) ? 14 : 16,
-    letterSpacing: isDeviceSize('medium', true) ? 0 : -0.48,
-    lineHeight: isDeviceSize('medium', true) ? 20 : 22,
-    color: colors.black,
-  },
-  localModalBodyTextBold: {
-    ...appStyles.medium16,
-    fontSize: isDeviceSize('medium', true) ? 14 : 16,
-    letterSpacing: isDeviceSize('medium', true) ? 0 : -0.48,
-    lineHeight: isDeviceSize('medium', true) ? 20 : 22,
-    color: colors.black,
-    fontWeight: 'bold',
-  },
-  bottom: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingBottom: 10,
-  },
-  underLine: {
-    borderBottomWidth: 1,
-    borderBottomColor: colors.lightGrey,
-  },
-  bottomText: {
-    ...appStyles.medium14,
-    lineHeight: 20,
-    color: colors.warmGrey,
-  },
-  popupNotice: {
-    marginTop: 16,
-  },
-  popupNoticeTitle: {
-    ...appStyles.bold16Text,
-    lineHeight: 24,
-    color: colors.darkBlue,
-    marginBottom: 2,
-  },
-  localNoticePopupIcon: {
-    alignSelf: 'center',
-    marginTop: 6,
-    marginBottom: 5,
-    borderWidth: 1,
-    borderColor: colors.whiteFive,
-    borderRadius: 10,
-
-    ...Platform.select({
-      ios: {
-        shadowColor: 'rgb(52, 62, 95)',
-        shadowOpacity: 0.2,
-        shadowRadius: 2,
-        shadowOffset: {
-          height: 1,
-          width: 1,
-        },
-      },
-      android: {
-        elevation: 3,
-      },
-    }),
-  },
-  localNoticePopupText: {
-    ...appStyles.semiBold14Text,
-    lineHeight: 20,
-    color: colors.warmGrey,
-    alignSelf: 'center',
-    marginBottom: 2,
-  },
 });
 
 type EsimScreenNavigationProp = StackNavigationProp<HomeStackParamList, 'Esim'>;
@@ -296,8 +188,6 @@ type EsimProps = {
     order: OrderAction;
     noti: NotiAction;
     cart: CartAction;
-  };
-  actions: {
     modal: ModalAction;
   };
 };
@@ -311,7 +201,6 @@ const Esim: React.FC<EsimProps> = ({
   product,
   account,
   noti,
-  actions,
 }) => {
   const [isDevModalVisible, setIsDevModalVisible] = useState<boolean>(true);
   const [index, setIndex] = useState(0);
@@ -435,120 +324,6 @@ const Esim: React.FC<EsimProps> = ({
     [action.product, navigation],
   );
 
-  const okHandler = useCallback(
-    (info: RkbPriceInfo) => {
-      actions.modal.closeModal();
-      navToCountry(info);
-    },
-    [actions.modal, navToCountry],
-  );
-
-  const localModal = useCallback(
-    (
-      info: RkbPriceInfo,
-      localOpName: string,
-      ccode: string[],
-      localOpKey: string,
-    ) => {
-      return (
-        <SafeAreaView style={{flex: 1}}>
-          <Pressable
-            style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.3)'}}
-            onPress={() => actions.modal.closeModal()}>
-            <Pressable
-              onPress={() => {}}
-              style={{
-                marginTop: 'auto',
-                paddingTop: 32,
-                paddingHorizontal: 20,
-                backgroundColor: 'white',
-                borderTopLeftRadius: 8,
-                borderTopRightRadius: 8,
-              }}>
-              <View style={styles.localModalTitle}>
-                <AppText style={styles.localModalTitleText}>
-                  {i18n.t('local:modal:title', {
-                    localOpName,
-                  })}
-                </AppText>
-              </View>
-              {(ccode.includes('TH') ? [1, 2] : [1]).map((k) => (
-                <View style={styles.localModalBody} key={k}>
-                  <View style={{flex: 1}}>
-                    <View style={styles.row}>
-                      <AppSvgIcon
-                        style={styles.localModalBodyIcon}
-                        name={k === 1 ? 'localNotice1' : 'localNotice2'}
-                      />
-                      <AppText style={styles.localModalBodyTitle}>
-                        {i18n.t(
-                          `local:modal:notice${k}${k === 2 ? ':th' : ''}:title`,
-                        )}
-                      </AppText>
-                    </View>
-                    <AppStyledText
-                      text={i18n.t(
-                        `local:modal:notice${k}${k === 2 ? ':th' : ''}:body`,
-                      )}
-                      textStyle={styles.localModalBodyText}
-                      format={{b: styles.localModalBodyTextBold}}
-                    />
-                    {!ccode.includes('TH') && (
-                      <View style={styles.popupNotice}>
-                        <AppText style={styles.popupNoticeTitle}>
-                          {i18n.t('local:modal:notice2:title')}
-                        </AppText>
-                        <AppStyledText
-                          text={i18n.t('local:modal:notice2:body')}
-                          textStyle={styles.localModalBodyText}
-                          format={{b: styles.localModalBodyTextBold}}
-                        />
-
-                        <View style={styles.localNoticePopupIcon}>
-                          <AppSvgIcon name="localNoticePopup" />
-                        </View>
-
-                        <AppText style={styles.localNoticePopupText}>
-                          {i18n.t('local:modal:popup:notice')}
-                        </AppText>
-                      </View>
-                    )}
-                  </View>
-                </View>
-              ))}
-              <View style={styles.okBtnContainer}>
-                <AppButton
-                  style={styles.okButton}
-                  title={i18n.t('local:ok')}
-                  type="primary"
-                  onPress={() => {
-                    okHandler(info);
-                  }}
-                />
-              </View>
-              <Pressable
-                style={styles.bottom}
-                onPress={() => {
-                  AsyncStorage.setItem(
-                    `esim.show.local.modal.${localOpKey}`,
-                    moment().format('YYYY-MM-DD HH:mm:ss'),
-                  );
-                  okHandler(info);
-                }}>
-                <View style={styles.underLine}>
-                  <AppText style={styles.bottomText}>
-                    {i18n.t('close:day')}
-                  </AppText>
-                </View>
-              </Pressable>
-            </Pressable>
-          </Pressable>
-        </SafeAreaView>
-      );
-    },
-    [actions.modal, okHandler],
-  );
-
   const onPressItem = useCallback(
     async (info: RkbPriceInfo) => {
       const localOp = product.localOpList.get(info?.partner || '');
@@ -561,12 +336,14 @@ const Esim: React.FC<EsimProps> = ({
         const tm = moment(item, 'YYYY-MM-DD HH:mm:ss');
 
         if (!tm.isValid() || tm.add(1, 'day').isBefore(moment())) {
-          return actions.modal.showModal({
-            content: localModal(
-              info,
-              localOpName,
-              localOp?.ccode || [],
-              localOp?.key || '',
+          return action.modal.showModal({
+            content: (
+              <LocalModal
+                onPress={() => navToCountry(info)}
+                localOpName={localOpName}
+                ccode={localOp?.ccode || []}
+                localOpKey={localOp?.key || ''}
+              />
             ),
           });
         }
@@ -574,7 +351,7 @@ const Esim: React.FC<EsimProps> = ({
 
       navToCountry(info);
     },
-    [actions.modal, localModal, navToCountry, product.localOpList],
+    [action.modal, navToCountry, product.localOpList],
   );
 
   const onIndexChange = useCallback((idx: number) => setIndex(idx), []);
@@ -1111,8 +888,6 @@ export default connect(
       noti: bindActionCreators(notiActions, dispatch),
       order: bindActionCreators(orderActions, dispatch),
       cart: bindActionCreators(cartActions, dispatch),
-    },
-    actions: {
       modal: bindActionCreators(modalActions, dispatch),
     },
   }),
