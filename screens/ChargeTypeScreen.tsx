47d8db9f433983f89a0a830f733c66711d647ed2
leeyounsoek05 <>
2023-05-08 08:59:22 +0900

    쿼드셀 상태값 처리

--- screens/ChargeTypeScreen.tsx
+++ screens/ChargeTypeScreen.tsx
@@ -122,21 +122,58 @@ const ChargeTypeScreen: React.FC<ChargeTypeScreenProps> = ({
     [chargedSubs, mainSubs],
   );
 
+  const checkQuadcellStatus = useCallback(async (item: RkbSubscription) => {
+    if (item?.imsi) {
+      const status = await API.Subscription.quadcellGetData({
+        imsi: item.imsi,
+        key: 'packlist',
+      });
+
+      const dataPack = status.objects?.packList?.find(
+        (elm) =>
+          elm?.packOrderSn !== undefined && Number(elm?.packCode) <= 900000,
+      );
+
+      if (status.result === 0 && status.objects?.retCode === '000000') {
+        const exp = moment(dataPack?.expTime, 'YYYYMMDDHHmmss').add(1, 'h');
+
+        // 사용 완료
+        if (!dataPack) {
+          setStatus('expired');
+          return;
+        }
+        if (dataPack?.effTime) {
+          if (moment().isAfter(exp)) {
+            // 사용 완료
+            setStatus('expired');
+            return;
+          }
+          // 사용 중
+          setStatus('using');
+          return;
+        }
+        // 사용 전
+        setStatus('unUsed');
+      }
+    }
+  }, []);
+
   useEffect(() => {
     switch (mainSubs.partner) {
       case 'cmi':
         checkCmiStatus(mainSubs);
         break;
       case 'quadcell':
+        checkQuadcellStatus(mainSubs);
         break;
       default:
         break;
     }
-  }, [checkCmiStatus, mainSubs]);
+  }, [checkCmiStatus, checkQuadcellStatus, mainSubs]);
 
   useEffect(() => {
-    console.log('@@@@ status', status);
-  }, [status]);
+    console.log('@@@@ partner, status', mainSubs.partner, status);
+  }, [mainSubs.partner, status]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -150,11 +187,11 @@ const ChargeTypeScreen: React.FC<ChargeTypeScreenProps> = ({
                 mainSubs: chargeableItem || mainSubs,
                 chargeablePeriod,
               });
-            } else if (status !== 'expired') {
+            } else if (status && status !== 'expired') {
               navigation.navigate('AddOn', {
                 mainSubs,
                 chargeablePeriod,
-                status
+                status,
               });
             }
           }}
