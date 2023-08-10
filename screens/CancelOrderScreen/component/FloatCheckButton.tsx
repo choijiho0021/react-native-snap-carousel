9b099d8706234cff8b4fbeb0d5cc77bf55867570
choijiho <choijiho0021@gmail.com>
2023-08-10 10:48:30 +0900

    발권-취소하기 체크박스 디자인 수정, 문구 점 추가

--- screens/CancelOrderScreen/component/FloatCheckButton.tsx
+++ screens/CancelOrderScreen/component/FloatCheckButton.tsx
@@ -9,29 +9,30 @@ import {sliderWidth} from '@/constants/SliderEntry.style';
 const styles = StyleSheet.create({
   agreement: {
     marginBottom: 12,
-    marginHorizontal: 20,
+    marginHorizontal: 8,
     position: 'absolute',
-    bottom: 0,
+    bottom: 20,
     backgroundColor: colors.white,
     padding: 20,
     display: 'flex',
     flexDirection: 'row',
-    width: sliderWidth - 40,
+    width: sliderWidth - 15,
     alignItems: 'center',
     borderWidth: 1,
     borderColor: colors.whiteFive,
+    borderRadius: 3,
 
-    elevation: 12,
+    elevation: 10,
     shadowColor: 'rgb(166, 168, 172)',
-    shadowRadius: 12,
-    shadowOpacity: 0.9,
+    shadowRadius: 10,
+    shadowOpacity: 0.44,
     shadowOffset: {
-      height: 4,
+      height: 0,
       width: 0,
     },
   },
   agreementText: {
-    marginRight: 40,
+    marginRight: 28,
     ...appStyles.semiBold16Text,
     lineHeight: 20,
   },
@@ -55,7 +56,7 @@ const FloatCheckButton: React.FC<FloatCheckButtonPros> = ({
       }}
       style={styles.agreement}>
       <AppIcon
-        style={{marginRight: 20}}
+        style={{marginRight: 12}}
         name="btnCheck2"
         checked={checked}
         size={22}
