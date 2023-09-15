07e6ad6f069ef10656e347aae9ee4e4efdecf249
Yeonsoo Joung <ysjoung@uangel.com>
2023-09-15 10:50:22 +0900

    80이상 사용 확인 로직 버그 수정

--- screens/EsimScreen/components/UsageItem.tsx
+++ screens/EsimScreen/components/UsageItem.tsx
@@ -163,11 +163,10 @@ const UsageItem: React.FC<UsageItemProps> = ({
   const [totalUsed, setTotalUsed] = useState<number>(usage?.totalUsed || 0);
   const isExhausted = useMemo(() => remain <= 0, [remain]);
   const isLowRemain = useMemo(
-    () => remain && quota >= 0 && Math.floor(remain / quota) <= 0.2,
+    () => quota >= 0 && remain / quota <= 0.2,
     [quota, remain],
   );
   const circularProgress = useRef();
-  const [isAnimated, setIsAnimated] = useState<boolean>(false);
 
   // const showUsage = useMemo(
   //   () =>
@@ -213,7 +212,7 @@ const UsageItem: React.FC<UsageItemProps> = ({
       console.log('@@ show snackbar');
       showSnackbar();
     }
-  }, [dataStatusCd, isAnimated, quota, remain, showSnackbar, usage, used]);
+  }, [dataStatusCd, quota, remain, showSnackbar, usage, used]);
 
   const renderResetTimeRow = useCallback(
     (key: string, rowStyle: ViewStyle = {}) => {
