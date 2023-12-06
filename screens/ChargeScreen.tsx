d987dc56ad96634d2c53013c4ce9041d766f9d5e
Yeonsoo Joung <ysjoung@uangel.com>
2023-12-06 15:58:05 +0900

    연장하기 상품리스트에  extLocalOps에 정의된 ID도 같이 조회하도록 변경

--- screens/ChargeScreen.tsx
+++ screens/ChargeScreen.tsx
@@ -87,10 +87,14 @@ const ChargeScreen: React.FC<ChargeScreenProps> = ({
 
   const prodData = useMemo(() => {
     if (mainSubs?.localOpId) {
-      return makeProdData(prodList, prodByLocalOp, [mainSubs?.localOpId]);
+      return makeProdData(
+        prodList,
+        prodByLocalOp,
+        [mainSubs?.localOpId].concat(mainSubs?.extLocalOps || []),
+      );
     }
     return [];
-  }, [mainSubs?.localOpId, prodByLocalOp, prodList]);
+  }, [mainSubs.extLocalOps, mainSubs?.localOpId, prodByLocalOp, prodList]);
 
   useEffect(() => {
     if (mainSubs?.localOpId) {
