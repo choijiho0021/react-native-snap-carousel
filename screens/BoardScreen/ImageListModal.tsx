6d01d9c1ca48cb528eb59bf22a5a723e57e5fd08
choijiho <choijiho0021@gmail.com>
2024-01-26 14:43:41 +0900

    1:1문의, 이벤트인증 내역 사진 미리보기 ZFlip에서 못 닫는 현상 수정

--- screens/BoardScreen/ImageListModal.tsx
+++ screens/BoardScreen/ImageListModal.tsx
@@ -1,4 +1,5 @@
 import {
+  Dimensions,
   Image,
   Modal,
   Pressable,
@@ -6,11 +7,12 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import React, {memo, useEffect, useState} from 'react';
+import React, {memo, useEffect, useMemo, useState} from 'react';
 import AppActivityIndicator from '@/components/AppActivityIndicator';
 import AppSvgIcon from '@/components/AppSvgIcon';
 import {colors} from '@/constants/Colors';
 import {
+  isFolderOpen,
   sliderWidth,
   windowHeight,
   windowWidth,
@@ -26,15 +28,13 @@ const styles = StyleSheet.create({
   },
   closeBtn: {
     position: 'absolute',
-    top: (windowHeight - ((sliderWidth - 40) / 9) * 16) / 2 - 29,
     width: '100%',
     alignItems: 'flex-end',
     paddingRight: 20,
+    zIndex: 100,
   },
   arrowLeft: {
     position: 'absolute',
-    width: (sliderWidth - 40) / 2,
-    height: ((sliderWidth - 40) / 9) * 16,
     alignItems: 'flex-start',
     left: 25,
     zIndex: 1,
@@ -42,16 +42,12 @@ const styles = StyleSheet.create({
   },
   arrowRight: {
     position: 'absolute',
-    width: (sliderWidth - 40) / 2,
-    height: ((sliderWidth - 40) / 9) * 16,
     alignItems: 'flex-end',
     right: 25,
     zIndex: 1,
     justifyContent: 'center',
   },
   modalImg: {
-    width: sliderWidth - 40,
-    height: ((sliderWidth - 40) / 9) * 16,
     backgroundColor: colors.black,
     alignItems: 'center',
     justifyContent: 'center',
@@ -76,6 +72,18 @@ const ImageListModal = ({
 }) => {
   const [imgIndex, setImgIndex] = useState(defaultImgIndex || 0);
   const [loading, setLoading] = useState(true);
+  const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({window}) => {
+      setDimensions(window);
+    });
+    return () => subscription?.remove();
+  }, []);
+
+  const isFolder = isFolderOpen(dimensions.width);
+
+  const width = useMemo(() => (isFolder ? 400 : sliderWidth), [isFolder]);
 
   useEffect(() => {
     if (defaultImgIndex !== undefined) {
@@ -87,10 +95,23 @@ const ImageListModal = ({
   return (
     <Modal visible={visible} transparent>
       <SafeAreaView style={styles.imgModalFrame}>
-        <View style={styles.closeBtn}>
+        <View
+          style={[
+            styles.closeBtn,
+            {
+              top: (windowHeight - ((width - 40) / 9) * 16) / 2 - 29,
+            },
+          ]}>
           <AppSvgIcon name="xWhite26" onPress={onPress} />
         </View>
-        <View style={styles.modalImg}>
+        <View
+          style={[
+            styles.modalImg,
+            {
+              width: width - 40,
+              height: ((width - 40) / 9) * 16,
+            },
+          ]}>
           {images?.[imgIndex] ? (
             <Image
               style={styles.imageFile}
@@ -103,7 +124,13 @@ const ImageListModal = ({
         </View>
         {images && images.length > 1 && imgIndex > 0 && (
           <Pressable
-            style={styles.arrowLeft}
+            style={[
+              styles.arrowLeft,
+              {
+                width: (width - 40) / 2,
+                height: ((width - 40) / 9) * 16,
+              },
+            ]}
             onPress={() => {
               setLoading(true);
               setImgIndex((prev) => prev - 1);
@@ -114,7 +141,13 @@ const ImageListModal = ({
 
         {images && images.length > 1 && imgIndex < images.length - 1 && (
           <Pressable
-            style={styles.arrowRight}
+            style={[
+              styles.arrowRight,
+              {
+                width: (width - 40) / 2,
+                height: ((width - 40) / 9) * 16,
+              },
+            ]}
             onPress={() => {
               setLoading(true);
               setImgIndex((prev) => prev + 1);
