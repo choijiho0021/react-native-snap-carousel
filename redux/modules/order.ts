167bd24a7e69153997f938061a2b2c72aa7e7f64
choijiho <choijiho0021@gmail.com>
2024-01-30 16:53:12 +0900

    actDate 받는 시간 미국시간대, 하루 끝으로 고정

--- redux/modules/order.ts
+++ redux/modules/order.ts
@@ -42,11 +42,11 @@ const defaultReturn = (resp) => {
 };
 
 export const getLastExpireDate = (sub: RkbSubscription) => {
+  // TODO : 서버에서 HT일땐 타임존 EST 붙여서 주도록 변경하기, 지금은 +00:00
   const lastExpireDate = isHt(sub)
-    ? moment(sub?.activationDate)
+    ? moment(`${sub?.activationDate?.toString().slice(0, 10)}T23:59:59-05:00`)
         ?.add(Number(sub?.prodDays) - 1, 'days')
-        .tz('EST')
-        .endOf('day')
+        .format()
     : sub?.lastExpireDate;
 
   return moment(lastExpireDate);
