55161bda0c25e0adc26fdee57f717f3b1c30fc35
leeyounsoek05 <>
2024-01-31 13:36:37 +0900

    바디부분 코드 분리

--- screens/ProductDetailScreen/index.tsx
+++ screens/ProductDetailScreen/index.tsx
@@ -24,8 +24,6 @@ import {
   TrackingStatus,
 } from 'react-native-tracking-transparency';
 import {ScrollView} from 'react-native-gesture-handler';
-import BeautifulSoup from 'beautiful-soup-js';
-import {SoupElement} from 'beautiful-soup-js/build/es/main/SoupElement';
 import AppAlert from '@/components/AppAlert';
 import AppBackButton from '@/components/AppBackButton';
 import {colors} from '@/constants/Colors';
@@ -63,7 +61,7 @@ import ChargeInfoModal from './components/ChargeInfoModal';
 import TextWithDot from '../EsimScreen/components/TextWithDot';
 import TextWithCheck from '../HomeScreen/component/TextWithCheck';
 import BackbuttonHandler from '@/components/BackbuttonHandler';
-import AppCopyBtn from '@/components/AppCopyBtn';
+import ProductDetailBody from './components/ProductDetailBody';
 
 const {esimGlobal, isIOS} = Env.get();
 const PURCHASE_LIMIT = 10;
@@ -288,32 +286,6 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     color: colors.white,
   },
-  dotBlack: {
-    ...appStyles.bold14Text,
-    marginHorizontal: 5,
-    lineHeight: 18,
-    color: colors.black,
-  },
-  noticeTextBlackWithDot: {
-    ...appStyles.normal14Text,
-    lineHeight: 18,
-    color: colors.warmGrey,
-  },
-  noticeTextBlack: {
-    ...appStyles.normal14Text,
-    lineHeight: 18,
-    color: colors.warmGrey,
-  },
-  noticeTextBlackBold: {
-    ...appStyles.semiBold14Text,
-    lineHeight: 18,
-    color: colors.black,
-  },
-  noticeTextBlueBold: {
-    ...appStyles.semiBold14Text,
-    lineHeight: 18,
-    color: colors.clearBlue,
-  },
   noticeText: {
     ...appStyles.normal14Text,
     lineHeight: 20,
@@ -412,148 +384,14 @@ const styles = StyleSheet.create({
     lineHeight: 22,
     color: colors.warmGrey,
   },
-  bodyTop: {
-    paddingVertical: 56,
-    paddingHorizontal: 20,
-    backgroundColor: colors.white,
-  },
-  bodyTopTitle: {
-    ...appStyles.medium20,
-    lineHeight: 22,
-    color: colors.black,
-  },
-  bodyTopBox: {
-    borderColor: colors.lightGrey,
-    borderWidth: 1,
-    borderRadius: 3,
-    padding: 20,
-    marginVertical: 16,
-  },
-  bodyTopBoxCountry: {
-    ...appStyles.semiBold16Text,
-    lineHeight: 22,
-    color: colors.black,
-    marginBottom: 4,
-  },
-  bodyTopBoxTel: {
-    ...appStyles.semiBold16Text,
-    lineHeight: 22,
-    color: colors.clearBlue,
-  },
-  multiApn: {
-    paddingVertical: 9,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  apnButton: {
-    display: 'flex',
-    flexDirection: 'row',
-    gap: 4,
-    alignItems: 'center',
-  },
-  apnButtonText: {
-    ...appStyles.medium14,
-    letterSpacing: 0.23,
-    color: colors.clearBlue,
-  },
-  tplInfo: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 8,
-    marginBottom: 16,
-  },
-  apnSetBox: {
-    marginTop: 40,
-    marginBottom: 16,
-  },
-  apnSetBoxTop: {
-    borderTopLeftRadius: 3,
-    borderTopRightRadius: 3,
-    paddingVertical: 14,
-    paddingHorizontal: 20,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  apnCopy: {
-    padding: 20,
-    backgroundColor: colors.whiteTwo,
-    borderBottomLeftRadius: 3,
-    borderBottomRightRadius: 3,
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.lightGrey,
-    borderRightColor: colors.lightGrey,
-    borderLeftColor: colors.lightGrey,
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  apn: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  apnBoxTitle: {
-    ...appStyles.normal18Text,
-    lineHeight: 22,
-    color: colors.white,
-  },
-  apnInfo: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  underline: {
-    borderBottomColor: colors.black,
-    borderBottomWidth: 1,
-    paddingBottom: 1,
-  },
-  apnPrefix: {
-    ...appStyles.normal16Text,
-    lineHeight: 24,
-    color: colors.warmGrey,
-    paddingBottom: 1,
-  },
-  apnInfoText: {
-    ...appStyles.bold16Text,
-    lineHeight: 24,
-    color: colors.black,
-  },
-  goToApnText: {
-    ...appStyles.semiBold16Text,
-    lineHeight: 24,
-    color: colors.black,
-  },
-  bodyBottom: {
-    paddingTop: 40,
-    paddingBottom: 56,
-    paddingHorizontal: 20,
-    backgroundColor: colors.whiteTwo,
-  },
-  bodyNoticeContents: {
-    marginTop: 24,
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 26,
-  },
-  noticeTitle: {
-    ...appStyles.bold14Text,
-    lineHeight: 20,
-    color: colors.black,
-    marginBottom: 2,
-  },
 });
 
-type ProductDetailScreenNavigationProp = StackNavigationProp<
+export type ProductDetailScreenNavigationProp = StackNavigationProp<
   HomeStackParamList,
   'ProductDetail'
 >;
 
-type ProductDetailScreenRouteProp = RouteProp<
+export type ProductDetailScreenRouteProp = RouteProp<
   HomeStackParamList,
   'ProductDetail'
 >;
@@ -1293,268 +1131,6 @@ const ProductDetailScreen: React.FC<ProductDetailScreenProps> = ({
     onPressBtnPurchase,
   ]);
 
-  const renderTplInfo = useCallback(
-    (t: {class: string; tag: string; text: string}, lineHeight?: number) => {
-      const regex = /►.*◄/;
-      const text = t.text.replace(/\t|&nbsp;/g, '');
-      // console.log('@@@@ text', text);
-      return (
-        <Pressable
-          onPress={() => regex.test(text) && onMessage('moveToFaq')}
-          key={text}>
-          {t.class === 'txt_dot' ? (
-            <TextWithDot
-              key={text}
-              text={text}
-              boldStyle={{...styles.noticeTextBlackBold, lineHeight}}
-              secondBoldStyle={{...styles.noticeTextBlueBold, lineHeight}}
-              textStyle={{...styles.noticeTextBlackWithDot, lineHeight}}
-              dotStyle={{...styles.dotBlack, lineHeight}}
-              marginRight={20}
-            />
-          ) : (
-            <AppStyledText
-              text={text}
-              textStyle={{...styles.noticeTextBlack, lineHeight}}
-              format={{
-                b: {...styles.noticeTextBlackBold, lineHeight},
-                s: {...styles.noticeTextBlueBold, lineHeight},
-              }}
-            />
-          )}
-        </Pressable>
-      );
-    },
-    [onMessage],
-  );
-
-  const renderAPN = useCallback(
-    (apn: string) => (
-      <View style={styles.apn}>
-        <View style={styles.apnInfo}>
-          <AppText style={styles.apnPrefix}>
-            {i18n.t('prodDetail:body:top:apn')}
-          </AppText>
-          <View style={styles.underline}>
-            <AppText style={styles.apnInfoText}>{apn}</AppText>
-          </View>
-        </View>
-        <AppCopyBtn
-          title={i18n.t('copy')}
-          onPress={() => onMessage('copy', apn)}
-        />
-      </View>
-    ),
-    [onMessage],
-  );
-
-  const attachBTag = useCallback((el: SoupElement, orgText: string) => {
-    const boldList = el?.contents
-      ?.reduce((acc: {text: string; tag: string}[], cur) => {
-        const text = cur?.getText();
-        if (text !== '') {
-          acc?.push({text, tag: cur?.name});
-        }
-        return acc;
-      }, [])
-      ?.filter((b) => b.text !== '.');
-
-    return boldList && boldList?.length > 0
-      ? boldList?.reduce((acc, cur) => {
-          const regex = new RegExp(`(${cur?.text})`, 'g');
-          return cur?.tag === 'a'
-            ? acc?.replace(regex, '<s>$1</s>')
-            : acc?.replace(regex, '<b>$1</b>');
-        }, orgText)
-      : orgText;
-  }, []);
-
-  const renderBodyTop = useCallback(
-    (soup: BeautifulSoup) => {
-      const tplInfoTags = soup?.find({attrs: {class: 'tpl_info'}});
-      const tplList =
-        tplInfoTags?.contents.length > 1
-          ? tplInfoTags?.contents.map((c) => ({
-              class: c?.attrs?.class,
-              tag: c?.name,
-              text: attachBTag(c, c.getText()),
-            }))
-          : [
-              {
-                class: tplInfoTags?.contents[0]?.attrs?.class,
-                tag: tplInfoTags?.name,
-                text: attachBTag(
-                  tplInfoTags?.contents[0],
-                  tplInfoTags?.getText(),
-                ),
-              },
-            ];
-
-      const apnList = descData?.desc?.apn?.split(',');
-      const apnInfo = apnList?.[0]?.split('/');
-      const tel = apnInfo?.[1]?.replace(/&amp;/g, ' • ');
-      const apn = apnInfo?.[2].replace(/&amp;/g, '&');
-
-      return (
-        <View style={styles.bodyTop}>
-          <AppText style={styles.bodyTopTitle}>
-            {i18n.t('prodDetail:body:top:title')}
-          </AppText>
-          {apnList &&
-            apnInfo &&
-            (apnList?.length > 1 ? (
-              <Pressable
-                style={styles.bodyTopBox}
-                onPress={() => onMessage('apn')}>
-                <View style={styles.multiApn}>
-                  <AppText style={styles.bodyTopBoxCountry}>
-                    {prod?.name
-                      .split(' ')
-                      .filter((item) => !/(무제한|종량제|\d+일)/.test(item))
-                      .join(' ')}
-                  </AppText>
-                  <View style={styles.apnButton}>
-                    <AppText style={styles.apnButtonText}>
-                      {i18n.t('pym:detail')}
-                    </AppText>
-                    <AppIcon name="arrowRightBlue10" />
-                  </View>
-                </View>
-              </Pressable>
-            ) : (
-              <View style={styles.bodyTopBox}>
-                <AppText style={styles.bodyTopBoxCountry}>{apnInfo[0]}</AppText>
-                <AppText style={styles.bodyTopBoxTel}>{tel}</AppText>
-              </View>
-            ))}
-          {tplList && tplList.length > 0 && !!tplList[0]?.text && (
-            <View style={styles.tplInfo}>
-              {tplList.map((t) => renderTplInfo(t))}
-            </View>
-          )}
-          <View style={styles.apnSetBox}>
-            <View
-              style={[
-                styles.apnSetBoxTop,
-                {
-                  backgroundColor:
-                    prod?.field_daily === 'daily'
-                      ? colors.purpleishBlue
-                      : colors.violet,
-                },
-              ]}>
-              <AppIcon name="apn" />
-              <AppText style={styles.apnBoxTitle}>
-                {i18n.t('store:apn')}
-              </AppText>
-            </View>
-
-            {apnList &&
-              apnInfo &&
-              (apnList?.length > 1 ? (
-                <Pressable
-                  style={[
-                    styles.apnCopy,
-                    {
-                      backgroundColor: colors.white,
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                    },
-                  ]}
-                  onPress={() => onMessage('apn')}>
-                  <AppText style={styles.goToApnText}>
-                    {i18n.t('prodDetail:body:top:go:apn')}
-                  </AppText>
-                  <AppIcon name="iconArrowRightBlack10" />
-                </Pressable>
-              ) : (
-                <View
-                  style={[styles.apnCopy, {flexDirection: 'column', gap: 16}]}>
-                  {apn?.split('&').map((a) => renderAPN(a))}
-                </View>
-              ))}
-          </View>
-          {[1, 2].map((i) => (
-            <TextWithDot
-              key={`prodDetail:body:top:apn:notice${i}`}
-              text={i18n.t(`prodDetail:body:top:apn:notice${i}`)}
-              boldStyle={styles.noticeTextBlackBold}
-              textStyle={styles.noticeTextBlackWithDot}
-              dotStyle={styles.dotBlack}
-              marginRight={20}
-            />
-          ))}
-        </View>
-      );
-    },
-    [
-      attachBTag,
-      descData?.desc?.apn,
-      onMessage,
-      prod?.field_daily,
-      prod?.name,
-      renderAPN,
-      renderTplInfo,
-    ],
-  );
-
-  const renderBodyNotice = useCallback(
-    (t: {class: string; tag: string; text: string}) => {
-      return t.tag === 'dt' ? (
-        <AppText style={styles.noticeTitle}>{t.text}</AppText>
-      ) : (
-        renderTplInfo(t, 20)
-      );
-    },
-    [renderTplInfo],
-  );
-
-  const renderBodyBottom = useCallback(
-    (soup: BeautifulSoup) => {
-      const tplCautionTags = soup?.find({attrs: {class: 'box_notandum'}});
-
-      const tplClist = tplCautionTags?.contents?.filter(
-        (c) => c?.name === 'dl',
-      );
-
-      const textList = tplClist?.map((c) => {
-        return c?.contents.map((c) => ({
-          class: c?.attrs?.class,
-          tag: c?.name,
-          text: attachBTag(c, c.getText()),
-        }));
-      });
-
-      return (
-        <View style={styles.bodyBottom}>
-          <AppText style={{...styles.bodyTopTitle, fontWeight: 'bold'}}>
-            {i18n.t('prodDetail:Caution')}
-          </AppText>
-          <View style={styles.bodyNoticeContents}>
-            {textList &&
-              textList.length > 0 &&
-              textList.map((t) => (
-                <View key={t[0].text}>{t.map((c) => renderBodyNotice(c))}</View>
-              ))}
-          </View>
-        </View>
-      );
-    },
-    [attachBTag, renderBodyNotice],
-  );
-
-  const renderBody = useCallback(() => {
-    const soup = new BeautifulSoup(
-      `<html><body>${descData?.body}</body></html>`,
-    );
-    return (
-      <View>
-        {renderBodyTop(soup)}
-        {renderBodyBottom(soup)}
-      </View>
-    );
-  }, [descData?.body, renderBodyBottom, renderBodyTop]);
-
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.header}>
@@ -1572,7 +1148,15 @@ const ProductDetailScreen: React.FC<ProductDetailScreenProps> = ({
 
       <ScrollView style={{flex: 1}}>
         {renderProdDetail()}
-        {descData?.body && renderBody()}
+        {descData?.body && (
+          <ProductDetailBody
+            body={descData?.body}
+            onMessage={onMessage}
+            descApn={descData?.desc?.apn || ''}
+            prodName={prod?.name || ''}
+            isDaily={prod?.field_daily === 'daily'}
+          />
+        )}
       </ScrollView>
       {/* useNativeDriver 사용 여부가 아직 추가 되지 않아 warning 발생중 */}
       {purchaseButtonTab()}
