0d7924c1d8c53bbb2966fa9a555a483a750d4ea1
bhtak <bhtak@uangel.com>
2024-02-24 20:18:05 +0900

    add extra buttons

--- components/AppPaymentGateway/PymMethod.tsx
+++ components/AppPaymentGateway/PymMethod.tsx
@@ -8,6 +8,8 @@ import {isDeviceSize} from '@/constants/SliderEntry.style';
 import AppButton from '../AppButton';
 import AppIcon from '../AppIcon';
 import PymButtonList from './PymButtonList';
+import AppTextInput from '../AppTextInput';
+import {PaymentParams} from '@/navigation/navigation';
 
 const styles = StyleSheet.create({
   container: {
@@ -28,17 +30,42 @@ const styles = StyleSheet.create({
     borderColor: colors.gray,
     borderWidth: 1,
   },
+  input: {
+    height: 44,
+    borderColor: colors.gray,
+    borderWidth: 1,
+    color: colors.black,
+  },
 });
+
+export type PymMethodRef = {
+  getExtraInfo: () => PaymentParams['receipt'];
+};
 
 type PymMethodProps = {
   value: string;
   onPress: (kind: string) => void;
+  pymMethodRef: React.MutableRefObject<PymMethodRef | null>;
 };
 
-const PymMethod: React.FC<PymMethodProps> = ({value, onPress}) => {
+const PymMethod: React.FC<PymMethodProps> = ({
+  value,
+  onPress,
+  pymMethodRef,
+}) => {
   const [method, setMethod] = useState<'easy' | 'card' | 'vbank'>('easy');
   const [selected, setSelected] = useState('');
-  const [receipt, setReceipt] = useState('1');
+  const [idType, setIdType] = useState<'m' | 'c' | 'b'>('m');
+  const [id, setId] = useState('');
+  const [rcptType, setRcptType] = useState<'p' | 'b' | 'n'>('p');
+
+  useEffect(() => {
+    if (pymMethodRef) {
+      pymMethodRef.current = {
+        getExtraInfo: () => ({id, idType, type: rcptType}),
+      };
+    }
+  }, [id, idType, pymMethodRef, rcptType]);
 
   useEffect(() => {
     if (value?.startsWith('card')) setMethod('card');
@@ -103,12 +130,12 @@ const PymMethod: React.FC<PymMethodProps> = ({value, onPress}) => {
                   {i18n.t('pym:vbank:receipt')}
                 </AppText>
                 <View style={[styles.row, {marginVertical: 10}]}>
-                  {['1', '2', '3'].map((k) => (
+                  {(['p', 'b', 'n'] as const).map((k) => (
                     <Pressable
                       key={k}
                       style={[styles.row, {flex: 1}]}
-                      onPress={() => setReceipt(k)}>
-                      <AppIcon name="btnCheck" checked={k === receipt} />
+                      onPress={() => setRcptType(k)}>
+                      <AppIcon name="btnCheck" checked={k === rcptType} />
                       <AppText style={{flex: 1, marginLeft: 5}}>
                         {i18n.t(`pym:vbank:receipt:${k}`)}
                       </AppText>
@@ -118,6 +145,11 @@ const PymMethod: React.FC<PymMethodProps> = ({value, onPress}) => {
                 <AppText style={styles.title}>
                   {i18n.t('pym:vbank:receipt')}
                 </AppText>
+                <AppTextInput
+                  style={styles.input}
+                  value={id}
+                  onChangeText={setId}
+                />
               </View>
             )
           ) : null}
