4a9c216f0fb06dfb38f7faa005b045456b90b67e
ysjoung <ysjoung@uangel.com>
2024-05-27 16:21:24 +0900

    esim 구매목록 가져오는 API V2 사용

--- redux/api/api.ts
+++ redux/api/api.ts
@@ -119,7 +119,7 @@ const path = {
       user: 'api/v1/noti/user',
     },
     rokebi: {
-      subs: 'rokebi/subs',
+      subs: '/rokebi/v2/subs',
       usage: 'rokebi/usage/subs',
       call: 'rokebi/call',
       ota: 'rokebi/svc/ota',
