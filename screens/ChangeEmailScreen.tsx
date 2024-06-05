cf06d3178109996260c007260d3411ba23dc2899
ysjoung <ysjoung@uangel.com>
2024-06-05 16:46:30 +0900

    이메일 변경 실패 시 키보드를 안 보이게 하여 토스트 메세지가 보이도록 변경

--- screens/ChangeEmailScreen.tsx
+++ screens/ChangeEmailScreen.tsx
@@ -6,6 +6,7 @@ import {
   View,
   SafeAreaView,
   KeyboardAvoidingView,
+  Keyboard,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -120,10 +121,11 @@ const ChangeEmailScreen: React.FC<ChangeEmailScreenProps> = ({
 
   const changeEmail = useCallback(() => {
     actions.account.changeEmail(newEmail).then((rsp) => {
-      if (rsp.payload.result === 0) {
+      if (rsp?.payload?.result === 0) {
         actions.toast.push('changeEmail:saveInfo');
         navigation.goBack();
       } else {
+        Keyboard.dismiss();
         actions.toast.push('changeEmail:fail');
       }
     });
