48aadfba96c39ece129c30cf75085e4910414cb2
ysjoung <ysjoung@uangel.com>
2024-10-22 14:20:28 +0900

    android webRtc 추가

--- android/app/src/main/java/com/rokebiesim/MainApplication.java
+++ android/app/src/main/java/com/rokebiesim/MainApplication.java
@@ -14,6 +14,8 @@ import com.android.installreferrer.api.InstallReferrerStateListener;
 import com.android.installreferrer.api.ReferrerDetails;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactApplication;
+import com.oney.WebRTCModule.WebRTCModuleOptions;
+import com.oney.WebRTCModule.WebRTCModulePackage;
 import com.reactnativegooglesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lugg.RNCConfig.RNCConfigPackage;
@@ -210,6 +212,7 @@ public class MainApplication extends Application implements ReactApplication {
                             new FlipperPackage(),
                             new PagerViewPackage(),
                             new NetInfoPackage(),
+                            new WebRTCModulePackage(),
                             new RNCPickerPackage(),
                             new ReactNativeFirebaseAnalyticsPackage(),
                             new CookieManagerPackage(),
@@ -263,6 +266,10 @@ public class MainApplication extends Application implements ReactApplication {
         prefs = getSharedPreferences("Pref", MODE_PRIVATE);
 
         ChannelIO.initialize(this);
+
+        // Initialize the WebRTC module options.
+        WebRTCModuleOptions options = WebRTCModuleOptions.getInstance();
+        options.enableMediaProjectionService = true;
 
         if (BuildConfig.DEBUG || BuildConfig.NODE_ENV == "development") {
             NTrackerExt.enableDebugLog(true);
