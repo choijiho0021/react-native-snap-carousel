cc1ef6ac7906714dbd9829952eb9de0f046f0016
SOOJEONG1 <soojeongs0408@gmail.com>
2024-11-19 14:03:11 +0900

    안드로이드 색상 안보이는 이슈 수정

--- screens/TalkContact/components/EmptyResult.tsx
+++ screens/TalkContact/components/EmptyResult.tsx
@@ -3,6 +3,8 @@ import {StyleSheet, View} from 'react-native';
 import AppIcon from '@/components/AppIcon';
 import AppText from '@/components/AppText';
 import i18n from '@/utils/i18n';
+import {colors} from '@/constants/Colors';
+import {appStyles} from '@/constants/Styles';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,13 +13,20 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
   },
+  title: {
+    ...appStyles.normal16Text,
+    lineHeight: 24,
+    color: colors.warmGrey,
+  },
 });
 
 const EmptyResult = ({title}: {title?: string}) => {
   return (
     <View style={styles.container}>
       <AppIcon style={{marginBottom: 16}} name="imgDot" />
-      <AppText>{title || i18n.t('talk:search:none')}</AppText>
+      <AppText style={styles.title}>
+        {title || i18n.t('talk:search:none')}
+      </AppText>
     </View>
   );
 };
