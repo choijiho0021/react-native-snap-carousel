2c7dcde86aba4fcaacf21da33f96dff51b24b5c2
SOOJEONG1 <soojeongs0408@gmail.com>
2024-12-24 12:03:53 +0900

    통화품질모달  - 키보드on > 모달 위로 올라가게 적용  - 키보드on > 다른곳 클릭시 키보드off  - 별점클릭시 확인버튼 활성화  (별점은 필수, 내용은 선택값)  - 하단에 탭안보이도록 적용

--- screens/RkbTalk/component/CallReviewModal.tsx
+++ screens/RkbTalk/component/CallReviewModal.tsx
@@ -1,35 +1,22 @@
 import {RootState} from '@reduxjs/toolkit';
 import React, {useCallback, useRef, useState} from 'react';
-import {InputAccessoryView, SafeAreaView, StyleSheet, View} from 'react-native';
+import {Keyboard, Pressable, StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import _ from 'underscore';
 import AppButton from '@/components/AppButton';
 import AppText from '@/components/AppText';
+import AppTextInput from '@/components/AppTextInput';
 import {colors} from '@/constants/Colors';
 import {appStyles} from '@/constants/Styles';
 import i18n from '@/utils/i18n';
-import AppTextInput from '@/components/AppTextInput';
-import Env from '@/environment';
-
-const {isIOS} = Env.get();
+import AnimatedModal from './AnimatedModal';
 
 const styles = StyleSheet.create({
   storeBox: {
-    position: 'absolute',
-    paddingTop: 32,
-    paddingBottom: 40,
     backgroundColor: colors.white,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
-    borderColor: colors.line,
-    shadowColor: colors.black8,
     height: 396,
-    bottom: 0,
     width: '100%',
   },
   modalMargin: {
-    flex: 1,
     marginHorizontal: 20,
     marginTop: 32,
   },
@@ -37,7 +24,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    marginBottom: 24,
   },
   headTitle: {
     ...appStyles.bold20Text,
@@ -45,12 +31,13 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
   },
   starRow: {
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
+    height: 38,
+    marginTop: 24,
+    marginBottom: 28,
   },
   inputBox: {
-    marginTop: 28,
     height: 128,
     borderRadius: 3,
     borderColor: colors.lightGrey,
@@ -70,26 +57,19 @@ const styles = StyleSheet.create({
     height: 52,
     backgroundColor: colors.white,
   },
-  inputAccessory: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    backgroundColor: colors.lightGrey,
-    padding: 5,
-  },
-  inputAccessoryText: {
-    ...appStyles.normal18Text,
-    textAlign: 'center',
-    margin: 5,
-  },
 });
 
 type CallReviewModalProps = {
+  visible: boolean;
   onPress: () => void;
 };
 
-const inputAccessoryViewID = 'doneKbd';
-const CallReviewModal: React.FC<CallReviewModalProps> = ({onPress}) => {
+const CallReviewModal: React.FC<CallReviewModalProps> = ({
+  visible = true,
+  onPress,
+}) => {
   const [star, setStar] = useState(0);
+  const [focused, setFocused] = useState(false);
   const [msg, setMsg] = useState<string>();
   const keybd = useRef();
 
@@ -104,89 +84,78 @@ const CallReviewModal: React.FC<CallReviewModalProps> = ({onPress}) => {
   }, []);
 
   return (
-    <View style={{flex: 1}}>
-      <SafeAreaView key="modal" style={styles.storeBox}>
-        <KeyboardAwareScrollView
-          enableOnAndroid
-          showsVerticalScrollIndicator={false}
-          extraScrollHeight={isIOS ? -100 : -300}>
-          <View style={styles.modalMargin}>
-            {head()}
-            <View style={styles.starRow}>
-              {[1, 2, 3, 4, 5].map((s) => (
-                <AppButton
-                  checked={s <= star}
-                  style={{marginHorizontal: 6}}
-                  iconName="btnStar"
-                  onPress={() => setStar((prev) => (prev === s ? 0 : s))}
-                />
-              ))}
-            </View>
-            <View style={{}} />
-            <AppTextInput
-              style={[
-                styles.inputBox,
-                msg ? {borderColor: colors.clearBlue} : undefined,
-              ]}
-              ref={keybd}
-              placeholder={i18n.t('talk:review:modal:placeholder')}
-              placeholderTextColor={colors.greyish}
-              multiline
-              numberOfLines={8}
-              inputAccessoryViewID={inputAccessoryViewID}
-              enablesReturnKeyAutomatically
-              clearTextOnFocus={false}
-              maxLength={2000}
-              onChangeText={(v) => {
-                setMsg(v);
-                // validate('msg', v);
-              }}
-              // onFocus={() => setExtraHeight(80)}
-              // error={error('msg')}
-              autoCapitalize="none"
-              autoCorrect={false}
-              // onContentSizeChange={({target}) =>
-              //   scrollRef.current?.props?.scrollToFocusedInput(
-              //     findNodeHandle(target),
-              //   )
-              // }
-              value={msg}
-            />
+    <AnimatedModal
+      containerStyle={styles.storeBox}
+      innerRadius={8}
+      keyboardFocused={focused}
+      bottomColor={colors.white}
+      animationType="slide"
+      visible={visible}
+      onClose={() => Keyboard.dismiss()}>
+      <Pressable style={styles.modalMargin} onPress={() => Keyboard.dismiss()}>
+        {head()}
+        <View style={styles.starRow}>
+          {[1, 2, 3, 4, 5].map((s) => (
             <AppButton
-              style={styles.okButton}
-              disabled
-              disableColor={colors.greyish}
-              disableBackgroundColor={colors.lightGrey}
-              title={i18n.t('ok')}
-              onPress={onPress}
+              checked={s <= star}
+              style={{marginHorizontal: 6}}
+              iconName="btnStar"
+              onPress={() => setStar(s)}
             />
-            <AppButton
-              style={styles.nextButton}
-              titleStyle={{
-                ...appStyles.bold16Text,
-                lineHeight: 24,
-                color: colors.black,
-              }}
-              title={i18n.t('talk:review:modal:next')}
-              onPress={onPress}
-            />
-          </View>
-          {isIOS ? (
-            <InputAccessoryView nativeID={inputAccessoryViewID}>
-              <AppButton
-                style={styles.inputAccessory}
-                title={i18n.t('done')}
-                titleStyle={[
-                  styles.inputAccessoryText,
-                  {color: _.isEmpty(msg) ? colors.white : colors.blue},
-                ]}
-                onPress={() => keybd.current?.blur()}
-              />
-            </InputAccessoryView>
-          ) : null}
-        </KeyboardAwareScrollView>
-      </SafeAreaView>
-    </View>
+          ))}
+        </View>
+        <AppTextInput
+          style={[
+            styles.inputBox,
+            msg ? {borderColor: colors.clearBlue} : undefined,
+          ]}
+          ref={keybd}
+          placeholder={i18n.t('talk:review:modal:placeholder')}
+          placeholderTextColor={colors.greyish}
+          multiline
+          numberOfLines={8}
+          enablesReturnKeyAutomatically
+          clearTextOnFocus={false}
+          maxLength={2000}
+          onEndEditing={() => Keyboard.dismiss()}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          onChangeText={(v) => {
+            setMsg(v);
+            // validate('msg', v);
+          }}
+          // error={error('msg')}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={msg}
+        />
+        <>
+          <AppButton
+            style={styles.okButton}
+            disabled={star === 0}
+            disableColor={colors.greyish}
+            disableBackgroundColor={colors.lightGrey}
+            titleStyle={{
+              ...appStyles.normal18Text,
+              lineHeight: 26,
+              color: star > 0 ? colors.white : colors.lightGrey,
+            }}
+            title={i18n.t('ok')}
+            onPress={onPress}
+          />
+          <AppButton
+            style={styles.nextButton}
+            titleStyle={{
+              ...appStyles.bold16Text,
+              lineHeight: 24,
+              color: colors.black,
+            }}
+            title={i18n.t('talk:review:modal:next')}
+            onPress={onPress}
+          />
+        </>
+      </Pressable>
+    </AnimatedModal>
   );
 };
 
